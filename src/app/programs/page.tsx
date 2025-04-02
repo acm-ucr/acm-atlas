ccc59c7d3d9c9f9fe5dfd9b81cfb274edff6a183
shuuvi <janeleebmes@gmail.com>
2025-04-02 16:48:54 -0700

    programs page text

---
+++ src/app/programs/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <p className="text-acm-gray-500 flex h-screen w-screen items-center justify-center text-4xl font-bold">
+      WHAT WE OFFER
+    </p>
+  );
+};
+
+export default Page;
