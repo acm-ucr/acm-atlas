7d1cb877a28357cae3b94147f16e9f6a3098b5db
Vincent Dinh <vincent.dinh401@gmail.com>
2025-04-02 15:56:40 -0700

    p text DAS

--- src/app/programs/das/page.tsx
+++ src/app/programs/das/page.tsx
@@ -1,5 +1,9 @@
 const Page = () => {
-  return <div></div>;
+  return (
+    <p className="text-acm-gray-500 flex h-screen w-screen items-center justify-center text-[75px] font-bold">
+      DAS
+    </p>
+  );
 };
 
 export default Page;
