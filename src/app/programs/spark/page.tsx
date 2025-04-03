55a6495ebfc40aa4ee076e9f399f2e9ef6df6e07
Andres Lopez <alope759@ucr.edu>
2025-04-02 19:45:05 -0700

    Andres Lopez / Added p-tag on the SPARK page

--- src/app/programs/spark/page.tsx
+++ src/app/programs/spark/page.tsx
@@ -1,5 +1,9 @@
 const Page = () => {
-  return <div></div>;
+  return (
+    <p className="text-acm-gray-500 flex h-screen w-screen items-center justify-center text-3xl">
+      SPARK
+    </p>
+  );
 };
 
 export default Page;
