d0c5e86827b926a13d77217246487067a458869d
Andres Lopez <alope759@ucr.edu>
2025-04-02 19:53:28 -0700

    Updated text to be bold.

--- src/app/programs/spark/page.tsx
+++ src/app/programs/spark/page.tsx
@@ -1,6 +1,6 @@
 const Page = () => {
   return (
-    <p className="text-acm-gray-500 flex h-screen w-screen items-center justify-center text-3xl">
+    <p className="text-acm-gray-500 flex h-screen w-screen items-center justify-center text-4xl font-bold">
       SPARK
     </p>
   );
