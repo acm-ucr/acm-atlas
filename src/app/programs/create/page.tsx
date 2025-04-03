6b8d4d043dab57ff45d90181197bd0646f5115ba
lbojo006 <luisbojorquezgoal@gmail.com>
2025-04-03 14:27:02 -0700

    Added P tag to Create page

--- src/app/programs/create/page.tsx
+++ src/app/programs/create/page.tsx
@@ -1,5 +1,11 @@
 const Page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p className="text-acm-gray-500 mt-10 flex justify-center text-center text-4xl font-bold md:text-5xl">
+        CREATE
+      </p>
+    </div>
+  );
 };
 
 export default Page;
