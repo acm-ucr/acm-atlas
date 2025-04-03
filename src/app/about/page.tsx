82967a0dc4369cc0030034939e196bb80f6de191
Stuart Arief <stuartarief@Stuarts-MacBook-Pro.local>
2025-04-03 12:40:48 -0700

    Changed tag from page to AboutUs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-const Programs = () => {
+const AboutUs = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <p className="text-acm-gray-500 text-7xl font-bold">ABOUT US</p>
@@ -6,4 +6,4 @@ const Programs = () => {
   );
 };
 
-export default Programs;
+export default AboutUs;
