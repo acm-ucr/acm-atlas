eb475f729ea427e4216be961219238e439611f88
Yoshinao <yoshinaoshimono@gmail.com>
2025-04-02 23:03:44 -0700

    home page tite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World!!
+    <div className="flex h-screen w-screen flex-col items-center">
+      <p className="mt-15 w-1/2 bg-red-50 text-center text-6xl leading-25 font-semibold text-[#444444]">
+        ASSOCIATION OF COMPUTING MACHINERY
+      </p>
     </div>
   );
 };
