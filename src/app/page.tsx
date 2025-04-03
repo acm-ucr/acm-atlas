d04b3dd1b66187452f75118cc648ed73b1d52466
Yoshinao <yoshinaoshimono@gmail.com>
2025-04-02 23:04:56 -0700

    remove background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center">
-      <p className="mt-15 w-1/2 bg-red-50 text-center text-6xl leading-25 font-semibold text-[#444444]">
+      <p className="mt-15 w-1/2 text-center text-6xl leading-25 font-semibold text-[#444444]">
         ASSOCIATION OF COMPUTING MACHINERY
       </p>
     </div>
