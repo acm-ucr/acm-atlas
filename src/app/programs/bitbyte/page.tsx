74d171bd9c25050c72b6e034f34bcfbf9b7e3691
Joshua Schumperli <jschu051@ucr.edu>
2025-04-04 16:21:55 -0700

    BIT/BYTE Page

--- src/app/programs/bitbyte/page.tsx
+++ src/app/programs/bitbyte/page.tsx
@@ -1,5 +1,9 @@
 const Page = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-acm-gray-500 text-7xl font-bold">BIT/BYTE</p>
+    </div>
+  );
 };
 
 export default Page;
