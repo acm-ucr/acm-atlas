92999d34d32b0ef86c9d1bd6d7fcdfb3acc1bb50
Vincent Dinh <vincent.dinh401@gmail.com>
2025-04-04 16:52:54 -0700

    updated text size

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
   return (
-    <p className="text-acm-navy flex h-screen w-screen items-center justify-center text-[60px] font-bold">
+    <p className="text-acm-navy flex h-screen w-screen items-center justify-center text-4xl font-semibold">
       PAGE NOT FOUND
     </p>
   );
