df34694fb37bac82bf91ed2bb3a43e6a7ab26bea
Vincent Dinh <vincent.dinh401@gmail.com>
2025-04-04 16:40:54 -0700

    updated file location

--- src/app/programs/das/page.tsx
+++ src/app/programs/das/page.tsx
@@ -1,8 +1,10 @@
+import DasPage from "@/components/programs/das";
+
 const Page = () => {
   return (
-    <p className="text-acm-gray-500 flex h-screen w-screen items-center justify-center text-[75px] font-bold">
-      DAS
-    </p>
+    <div>
+      <DasPage />
+    </div>
   );
 };
 
