eb1278d6bd90d67925927108b228bd4049e35a9e
shuuvi <janeleebmes@gmail.com>
2025-04-07 16:45:41 -0700

    fixed merge conflicts again

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,7 +1,7 @@
 const Programs = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <p className="text-acm-gray-500 font-font-bold text-7xl">WHAT WE OFFER</p>
+      <p className="text-acm-gray-500 text-7xl font-bold">WHAT WE OFFER</p>
     </div>
   );
 };
