c70443a9f563b31899c10df6d6b68824d4798dc6
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-07 16:34:42 -0700

    Added styling

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
-    <div>
-      <p>MEET THE BOARD</p>
+    <div className="flex w-screen items-center justify-center p-14">
+      <p className="text-acm-gray-500 text-5xl font-bold">MEET THE BOARD</p>
     </div>
   );
 };
