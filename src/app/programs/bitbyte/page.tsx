447f16fdffafafbdb68bf5afc5be74e2096d68fc
Joshua Schumperli <jschu051@ucr.edu>
2025-04-07 19:58:16 -0700

    BIT/BYTE

--- src/app/programs/bitbyte/page.tsx
+++ src/app/programs/bitbyte/page.tsx
@@ -1,7 +1,8 @@
+import Header from "@/components/programs/bitbyte/header";
 const Page = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <p className="text-acm-gray-500 text-7xl font-bold">BIT/BYTE</p>
+    <div>
+      <Header />
     </div>
   );
 };
