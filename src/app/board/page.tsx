f0ad0ef37f7caa97508941999f6af0f487e7962a
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-07 21:43:06 -0700

    renamed to Header

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,5 +1,11 @@
+import Header from "@/components/board/Header";
+
 const Page = () => {
-  return <div> </div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Page;
