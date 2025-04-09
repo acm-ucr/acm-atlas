77d7a1ab0362be723696517274b8a08161c2f280
Yoshinao <yoshinaoshimono@gmail.com>
2025-04-09 12:36:18 -0700

    comments addressed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="flex w-screen flex-col items-center px-10">
-      <div className="bg-acm-blue-700 h-[2px] w-full"></div>
+    <div className="flex w-screen flex-col items-center gap-5 px-10">
+      <div className="bg-acm-blue-400 h-[2px] w-full" />
       <p className="text-acm-gray-500">
         © ACM at UCR 2025. Made with 💙 from ACM Spark.
       </p>
