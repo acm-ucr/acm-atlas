4b74944d464f6794ed7b664b93e6aa536a9dda94
Andres Lopez <alope759@ucr.edu>
2025-04-08 18:41:10 -0700

    Corrected text and button colors.

--- src/components/home/seemore.tsx
+++ src/components/home/seemore.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const SeeMoreButton = () => {
   return (
-    <button className="bg-acm-blue-500 text-md hover:bg-acm-navy absolute bottom-10 h-10 w-40 rounded-lg">
+    <button className="bg-acm-eslint text-md hover:bg-acm-typescript absolute bottom-10 h-10 w-40 rounded-lg text-white">
       <Link href="/events">See More</Link>
     </button>
   );
