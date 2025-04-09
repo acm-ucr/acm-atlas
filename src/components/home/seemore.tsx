1e23ba660f61df387766d85f21a010ffc14aa137
Andres Lopez <alope759@ucr.edu>
2025-04-09 15:31:15 -0700

    Updated src/components/home/seemore.tsx
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>
    Signed-off-by: Andres Lopez <alope759@ucr.edu>

--- src/components/home/seemore.tsx
+++ src/components/home/seemore.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const SeeMoreLink = () => {
   return (
-    <div className="fixed bottom-1/2 left-1/2 -translate-x-1/2">
+    <div className="flex justify-center">
       <Link
         href="/events"
         className="bg-acm-blue-700 flex w-3xs justify-center rounded-lg p-3 text-3xl font-medium text-white"
