7eb4b710ef0ba8f411731d8a6a8a46ee5e368c17
Joshua Schumperli <jschu051@ucr.edu>
2025-04-11 16:56:34 -0700

    p-tag for the Upcoming Events title on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ViewAllLink from "@/components/home/viewalllink";
 import Header from "@/components/home/header";
+import UpcomingEvents from "@/components/home/UpcomingEvents";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <ViewAllLink link="/programs/spark">Spark</ViewAllLink>
       <ViewAllLink link="/programs/create">Create</ViewAllLink>
       <ViewAllLink link="/programs/forge">Forge</ViewAllLink>
+      <UpcomingEvents />
     </div>
   );
 };
---
+++ src/components/home/UpcomingEvents.tsx
@@ -0,0 +1,11 @@
+const UpcomingEvents = () => {
+  return (
+    <div className="absolute top-4517 left-115">
+      <p className="text-acm-gray-500 text-7xl font-semibold">
+        Upcoming Events
+      </p>
+    </div>
+  );
+};
+
+export default UpcomingEvents;
