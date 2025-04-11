10a2987403bed0a75184968606966465ab287d96
Stuart Arief <stuartarief@Mac.attlocal.net>
2025-04-11 13:13:13 -0700

    Added Spark Arrowlink component into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import LinkArrowSpark from "@/components/home/arrowlink";
 import Header from "@/components/home/header";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <LinkArrowSpark link="/programs/spark"></LinkArrowSpark>
     </div>
   );
 };
