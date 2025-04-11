3eb661fc0f6368bfe3e2dee830c04dd560dd7f2a
Andres Lopez <alope759@ucr.edu>
2025-04-11 16:07:16 -0700

    Edited y padding.

--- src/components/home/seemorelink.tsx
+++ src/components/home/seemorelink.tsx
@@ -5,7 +5,7 @@ const SeeMoreLink = () => {
     <div className="flex justify-center">
       <Link
         href="/events"
-        className="bg-acm-blue-700 rounded-2xl p-3 px-12 text-3xl font-medium text-white"
+        className="bg-acm-blue-700 rounded-2xl px-12 py-3 text-3xl font-medium text-white"
       >
         See More
       </Link>
