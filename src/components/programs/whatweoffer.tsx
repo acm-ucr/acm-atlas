9cfcbd5db7486b66f2587ecef0a66a69f356e3d8
shuuvi <janeleebmes@gmail.com>
2025-04-13 17:13:17 -0700

    fixed issue with paragraph formatting

--- src/components/programs/whatweoffer.tsx
+++ src/components/programs/whatweoffer.tsx
@@ -26,26 +26,26 @@ const WhatWeOffer = ({
   return (
     <div className="mx-[20%] grid grid-cols-3">
       <div className="col-span-2 grid">
-        <div className="text-acm-gray-500 mr-[20%] grid grid-flow-col text-4xl font-bold text-nowrap">
+        <div className="text-acm-gray-500 flex text-4xl font-bold text-nowrap">
           <p className="">{header}</p>
-          <p className={`${textColor} font-bold`}>{program}</p>
+          <p className={`${textColor} font-bold`}>&nbsp;{program}&nbsp;</p>
           <p>?</p>
         </div>
 
-        <div className="text-acm-gray-500 flex text-2xl font-semibold text-balance">
-          <p>Join</p>
-          <p className="font-bold text-nowrap">&nbsp;{acm}&nbsp;</p>
-          <p>{pText}</p>
+        <div className="w-3/4 text-acm-gray-500 text-2xl font-semibold">
+          <p className="inline">Join</p>
+          <p className="inline font-bold">&nbsp;{acm}&nbsp;</p>
+          <p className="inline">{pText}</p>
         </div>
         <Link
           href={link}
-          className={`${linkColor} text-acm-gray-500 flex w-1/4 items-center justify-center rounded-lg pt-1 pb-1 text-2xl text-xl font-semibold`}
+          className={`${linkColor} text-acm-gray-500 flex w-1/4 items-center justify-center rounded-lg pt-1 pb-1 text-xl font-semibold`}
         >
           Learn More →
         </Link>
       </div>
 
-      <div className="it flex justify-center">
+      <div className="flex justify-center">
         <Image src={image} alt="acm" width={200} height={200} />
       </div>
     </div>
