8ff68b57e98434d63f67cdf0ec092fbdcfc70bc4
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-14 12:07:58 -0700

    removed button element

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,9 +6,12 @@ const NotFound = () => {
       <p className="text-acm-navy flex w-screen items-center justify-center text-4xl font-semibold">
         PAGE NOT FOUND
       </p>
-      <button className="bg-acm-blue-700 my-5 rounded-md px-7 py-2 font-semibold text-white">
-        <Link href="/">Go Back Home</Link>
-      </button>
+      <Link
+        className="bg-acm-blue-700 my-5 rounded-md px-7 py-2 font-semibold text-white"
+        href="/"
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 };
