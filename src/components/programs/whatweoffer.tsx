038f20c810d541d58f024c41734f6901f3e5ebb1
shuuvi <janeleebmes@gmail.com>
2025-04-13 17:36:35 -0700

    fixed all requested changes and comments

--- src/components/programs/whatweoffer.tsx
+++ src/components/programs/whatweoffer.tsx
@@ -24,29 +24,29 @@ const WhatWeOffer = ({
   link,
 }: ProgramProps) => {
   return (
-    <div className="mx-[20%] grid grid-cols-3">
-      <div className="col-span-2 grid">
+    <div className="mx-[15%] grid grid-cols-3">
+      <div className="col-span-2">
         <div className="text-acm-gray-500 flex text-4xl font-bold text-nowrap">
-          <p className="">{header}</p>
+          <p>{header}</p>
           <p className={`${textColor} font-bold`}>&nbsp;{program}&nbsp;</p>
           <p>?</p>
         </div>
 
-        <div className="text-acm-gray-500 w-3/4 text-2xl font-semibold">
+        <div className="text-acm-gray-500 w-5/6 py-2 text-2xl font-semibold">
           <p className="inline">Join</p>
           <p className="inline font-bold">&nbsp;{acm}&nbsp;</p>
           <p className="inline">{pText}</p>
         </div>
         <Link
           href={link}
-          className={`${linkColor} text-acm-gray-500 flex w-1/4 items-center justify-center rounded-lg pt-1 pb-1 text-xl font-semibold`}
+          className={`${linkColor} text-acm-gray-500 flex w-1/4 items-center justify-center rounded-lg py-2 text-xl font-semibold`}
         >
           Learn More →
         </Link>
       </div>
 
       <div className="flex justify-center">
-        <Image src={image} alt="acm" width={200} height={200} />
+        <Image src={image} alt="program" />
       </div>
     </div>
   );
