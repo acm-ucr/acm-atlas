dc87ee55a4a3984aafbc75602a4e414e1eb485f4
Shaodw709 <amirvs9675@gmail.com>
2025-04-13 19:36:11 -0700

    added pathway

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   NavigationMenu,
@@ -7,25 +8,42 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 const Navigation = () => {
+  const pathname = usePathname();
   return (
     <div className="border-acm-blue-400 grid grid-cols-5 border-b-3 p-5 shadow-xl">
-      <Link className="text-acm-gray-500 text-2xl font-bold" href="/">
+      <Link
+        className={
+          pathname === "/" ? "text-acm-gray-500 text-2xl font-semibold" : ""
+        }
+        href="/"
+      >
         ACM@UCR
       </Link>
       <div className="col-span-4 flex justify-end space-x-5 pr-5">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/about">ABOUT</Link>
+              <Link
+                href="/about"
+                className={pathname === "/about" ? "font-semibold" : ""}
+              >
+                ABOUT
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/board">BOARD</Link>
+              <Link
+                href="/board"
+                className={pathname === "/board" ? "font-semibold" : ""}
+              >
+                BOARD
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
@@ -34,19 +52,44 @@ const Navigation = () => {
             <NavigationMenuItem>
               <NavigationMenuTrigger>PROGRAMS</NavigationMenuTrigger>
               <NavigationMenuContent>
-                <NavigationMenuLink href="/programs/spark">
+                <NavigationMenuLink
+                  href="/programs/spark"
+                  className={
+                    pathname === "/programs/spark" ? "font-semibold" : ""
+                  }
+                >
                   SPARK
                 </NavigationMenuLink>
-                <NavigationMenuLink href="/programs/forge">
+                <NavigationMenuLink
+                  href="/programs/forge"
+                  className={
+                    pathname === "/programs/forge" ? "font-semibold" : ""
+                  }
+                >
                   FORGE
                 </NavigationMenuLink>
-                <NavigationMenuLink href="/programs/create">
+                <NavigationMenuLink
+                  href="/programs/create"
+                  className={
+                    pathname === "/programs/create" ? "font-semibold" : ""
+                  }
+                >
                   CREATE
                 </NavigationMenuLink>
-                <NavigationMenuLink href="/programs/das">
+                <NavigationMenuLink
+                  href="/programs/das"
+                  className={
+                    pathname === "/programs/das" ? "font-semibold" : ""
+                  }
+                >
                   DAS
                 </NavigationMenuLink>
-                <NavigationMenuLink href="/programs/bitbyte">
+                <NavigationMenuLink
+                  href="/programs/bitbyte"
+                  className={
+                    pathname === "/programs/bitbyte" ? "font-semibold" : ""
+                  }
+                >
                   Bit/Byte
                 </NavigationMenuLink>
               </NavigationMenuContent>
@@ -56,21 +99,36 @@ const Navigation = () => {
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/hacks">HACKS</Link>
+              <Link
+                href="/hacks"
+                className={pathname === "/hacks" ? "font-semibold" : ""}
+              >
+                HACKS
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/events">EVENTS</Link>
+              <Link
+                href="/events"
+                className={pathname === "/events" ? "font-semibold" : ""}
+              >
+                EVENTS
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/join">JOIN US</Link>
+              <Link
+                href="/join"
+                className={pathname === "/join" ? "font-semibold" : ""}
+              >
+                JOIN US
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
