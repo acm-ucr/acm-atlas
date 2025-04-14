72837c10e9bcc6c12af3f926ea02a682ef55fc5e
Sarbesh1234 <sankarsarbesh@gmail.com>
2025-04-14 02:51:13 -0700

    prettier?

--- src/components/home/currentopportunities.tsx
+++ src/components/home/currentopportunities.tsx
@@ -1,9 +1,9 @@
 const CurrentOpportunities = () => {
-    return (
-      <div className="flex h-screen w-screen items-center justify-center">
-        <p className="text-6xl font-bold text-gray-600">CURRENT OPPORTUNITIES</p>
-      </div>
-    );
-  };
- 
-  export default CurrentOpportunities;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <p className="text-6xl font-bold text-gray-600">CURRENT OPPORTUNITIES</p>
+    </div>
+  );
+};
+
+export default CurrentOpportunities;
