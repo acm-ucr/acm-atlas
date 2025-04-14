ae3fb8dac5f91d1d0dc818961f8c0feee853a695
Sarbesh1234 <sankarsarbesh@gmail.com>
2025-04-14 03:34:37 -0700

    changes

--- src/components/home/featuredprojects.tsx
+++ src/components/home/featuredprojects.tsx
@@ -1,10 +1,9 @@
 const FeaturedProjects = () => {
-    return (
-      <div className="flex h-screen w-screen items-center justify-center">
-        <p className="text-6xl font-bold text-gray-600">FEATURED PROJECTS</p>
-      </div>
-    );
-  };
-  
-  export default FeaturedProjects;
-  
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <p className="text-6xl font-bold text-gray-600">FEATURED PROJECTS</p>
+    </div>
+  );
+};
+
+export default FeaturedProjects;
