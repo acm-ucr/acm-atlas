89866a18239b6b0041e64dca3490efb86b7379a5
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-14 11:59:46 -0700

    implemented go back home button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 const NotFound = () => {
   return (
-    <p className="text-acm-navy flex h-screen w-screen items-center justify-center text-4xl font-semibold">
-      PAGE NOT FOUND
-    </p>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <p className="text-acm-navy flex w-screen items-center justify-center text-4xl font-semibold">
+        PAGE NOT FOUND
+      </p>
+      <button className="bg-acm-blue-700 my-5 rounded-md px-7 py-2 font-semibold text-white">
+        <Link href="/">Go Back Home</Link>
+      </button>
+    </div>
   );
 };
 
