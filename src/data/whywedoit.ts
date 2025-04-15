4443c04905d312f085e2187f7b10239e660ab18b
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-15 09:49:25 -0700

    created new data file

---
+++ src/data/whywedoit.ts
@@ -0,0 +1,7 @@
+export const WhyWeDoIt = [
+  {
+    header: "WHY WE DO IT",
+    ptext:
+      "We want UCR to become a premier institution for Computer Science and Engineering. University courses provide a good foundation, but we strive to build upon that. But more importantly, we want our members to make a positive difference in the world through software.",
+  },
+];
