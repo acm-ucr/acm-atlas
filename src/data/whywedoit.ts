076e0abd5e1099ff44b24430c3c3ff5c29182f3c
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-15 10:02:22 -0700

    Imported webp images

--- src/data/whywedoit.ts
+++ src/data/whywedoit.ts
@@ -1,3 +1,8 @@
+import Image1 from "@/public/WhyWeDoIt/image-1.webp";
+import Image2 from "@/public/WhyWeDoIt/image-2.webp";
+import Image3 from "@/public/WhyWeDoIt/image-3.webp";
+import Image4 from "@/public/WhyWeDoIt/image-4.webp";
+
 export const WhyWeDoIt = [
   {
     header: "WHY WE DO IT",
