4c8cbd5815877c9d59f79f5667411a9c118816e4
lbojo006 <luisbojorquezgoal@gmail.com>
2025-04-20 01:19:00 -0700

    Added Card Component (Still working on it)

---
+++ src/components/home/ProjectCard.tsx
@@ -0,0 +1,30 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+interface ProjectCardProps {
+  project: string;
+  image: StaticImageData;
+  link: string;
+}
+
+const ProjectCard = ({ project, image, link }: ProjectCardProps) => {
+  return (
+    <Link
+      href={link}
+      className="relative my-5 flex flex-row items-center overflow-hidden rounded-lg p-4"
+      style={{
+        backgroundImage: `url(${image.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute inset-0 backdrop-blur-sm"></div>
+      <div className="z-10">
+        <Image src={image} alt={`${project} Image`} />
+        <div className="mt-4 w-full rounded-4xl bg-white p-2">{project}</div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProjectCard;
--- src/components/home/featuredprojects.tsx
+++ src/components/home/featuredprojects.tsx
@@ -1,7 +1,24 @@
+import ProjectCard from "../home/ProjectCard";
+import ProjectImage from "@/public/Spark/aviatr.webp";
+import ProjectImage2 from "@/public/Spark/aag.webp";
+import ProjectImage3 from "@/public/Spark/robosub.webp";
+
 const FeaturedProjects = () => {
   return (
-    <div className="flex items-center justify-center">
-      <p className="text-acm-gray-500 text-6xl font-bold">FEATURED PROJECTS</p>
+    <div>
+      <div className="flex items-center justify-center">
+        <p className="text-acm-gray-500 text-6xl font-bold">
+          FEATURED PROJECTS
+        </p>
+      </div>
+      <div className="text-acm-gray-500 mx-10 mt-10 font-bold">
+        <p className="text-3xl">ACM SPARK</p>
+        <div className="grid grid-cols-3 gap-3 text-lg">
+          <ProjectCard project="AVIAT’R" image={ProjectImage} link="/" />
+          <ProjectCard project="AAG" image={ProjectImage2} link="/" />
+          <ProjectCard project="ROBO SUB" image={ProjectImage3} link="/" />
+        </div>
+      </div>
     </div>
   );
 };
