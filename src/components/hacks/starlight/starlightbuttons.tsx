c33e5b1c5040c6d60c08b4098ddba606b841adce
arkbot51 <rhahee528@gmail.com>
2025-04-20 15:54:42 -0700

    added text

--- src/components/hacks/starlight/starlightbuttons.tsx
+++ src/components/hacks/starlight/starlightbuttons.tsx
@@ -1,26 +1,34 @@
+import Link from "next/link";
+
 const Starlightbuttons = () => {
   return (
     <div className="flex justify-center p-10">
-      <div className="grid grid-flow-row grid-cols-2 gap-20">
+      <div className="grid grid-cols-2 gap-20">
         <div className="flex flex-col justify-center">
-          <a
+          <p className="text-acm-gray-500 pb-5 text-center text-2xl font-semibold">
+            WANT TO GET INVOLVED?
+          </p>
+          <Link
             href="https://discord.gg/5xzE2qTEKZ"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-acm-navy flex justify-center rounded-lg px-18 py-4 text-xl text-white"
           >
             Join our Discord!
-          </a>
+          </Link>
         </div>
         <div className="flex flex-col justify-center">
-          <a
+          <p className="text-acm-gray-500 pb-5 text-center text-2xl font-semibold">
+            CHECK OUT OUR WORK!
+          </p>
+          <Link
             href="https://github.com/acm-ucr/starlight"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-acm-navy flex justify-center rounded-lg px-18 py-4 text-xl text-white"
           >
             Github
-          </a>
+          </Link>
         </div>
       </div>
     </div>
