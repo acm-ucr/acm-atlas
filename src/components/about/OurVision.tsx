08b2876d3f13c961ddf082bad11d86da831af095
Andres Lopez <alope759@ucr.edu>
2025-04-21 14:24:34 -0700

    Updated formatting via suggested changes by lead.

--- src/components/about/OurVision.tsx
+++ src/components/about/OurVision.tsx
@@ -13,11 +13,13 @@ const OurVision = () => {
             <Image src={placeholder} alt="Placeholder Image 2" fill />
           </div>
         </div>
-        <div className="relative aspect-[1/1] w-2/3 overflow-hidden rounded-xl">
-          <Image src={placeholder} alt="Placeholder Image 3" fill />
-        </div>
-        <div className="relative aspect-[16/9] w-4/5 overflow-hidden rounded-xl">
-          <Image src={placeholder} alt="Placeholder Image 4" fill />
+        <div className="col-span-3 m-4 grid grid-rows-2 gap-4">
+          <div className="relative aspect-[1/1] w-auto overflow-hidden rounded-xl">
+            <Image src={placeholder} alt="Placeholder Image 3" fill />
+          </div>
+          <div className="relative mt-[-48] aspect-[16/9] w-auto overflow-hidden rounded-xl">
+            <Image src={placeholder} alt="Placeholder Image 4" fill />
+          </div>
         </div>
       </div>
       <div className="text-acm-gray-500 flex flex-col items-center text-center">
