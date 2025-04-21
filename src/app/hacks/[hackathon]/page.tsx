16598b9a42e8319cbb52c13ecca2f3ae9e1c12a2
stanleylew5 <stanley43595@gmail.com>
2025-04-20 18:43:04 -0700

    dynamic routing

--- src/app/hacks/[hackathon]/page.tsx
+++ src/app/hacks/[hackathon]/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Hack from "@/components/hacks/hack";
+import hacks from "@/data/hackathons";
+import NotFound from "@/app/not-found";
 
-const page = () => {
-  return <div></div>;
+const Page = ({ params }: { params: { hackathon: string } }) => {
+  const hackathon = hacks.find((hack) => hack.id === params.hackathon);
+  if (!hackathon) return <NotFound />;
+  const { name, website } = hackathon;
+  return <Hack name={name} website={website} />;
 };
 
-export default page;
+export default Page;
