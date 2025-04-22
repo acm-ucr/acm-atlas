bfd606ddc924df2347934a436e57f3a1a5fef7ba
Andres Lopez <alope759@ucr.edu>
2025-04-21 21:01:56 -0700

    Updated photos & code styling.

--- src/components/about/OurVision.tsx
+++ src/components/about/OurVision.tsx
@@ -1,48 +1,52 @@
 import Image from "next/image";
+const OurVision1 = "AboutUs/OurVision1.webp";
+const OurVision2 = "AboutUs/OurVision2.webp";
+const OurVision3 = "AboutUs/OurVision3.webp";
+const OurVision4 = "AboutUs/OurVision4.webp";
 
 const OurVision = () => {
   return (
-    <div className="ml-16 grid grid-cols-2">
+    <div className="mx-auto grid w-11/12 grid-cols-2">
       <div className="grid w-2/3 grid-cols-5">
         <div className="col-span-2 grid grid-rows-3 gap-6">
           <div className="relative mt-4 aspect-[1/1] w-auto overflow-hidden rounded-xl">
             <Image
-              src={"AboutUs/OurVision/1_1.webp"}
-              alt="ACM Members attend an in-progress general ACM meeting."
+              src={OurVision1}
+              alt="Image 1"
               fill
-              quality={85}
+              className="object-cover"
             />
           </div>
           <div className="relative row-span-2 h-[49.5%] w-auto overflow-hidden rounded-xl">
             <Image
-              src={"AboutUs/OurVision/1_3.webp"}
-              alt="A screen presenting ACM VP Jerry Li's LinkedIn is provided to members as the exemplar."
+              src={OurVision3}
+              alt="Image 3"
               fill
-              quality={85}
+              className="object-cover"
             />
           </div>
         </div>
         <div className="col-span-3 m-4 grid grid-rows-2 gap-4">
           <div className="relative aspect-[1/1] w-auto overflow-hidden rounded-xl">
             <Image
-              src={"AboutUs/OurVision/1_2.webp"}
-              alt="ACM members and UCR students alike attend an ACM workshop on the basics of Python programming at the Pentland Hills Foxhole."
+              src={OurVision2}
+              alt="Image 2"
               fill
-              quality={85}
+              className="object-cover"
             />
           </div>
           <div className="relative mt-[-48] aspect-[15/9] w-auto overflow-hidden rounded-xl">
             <Image
-              src={"AboutUs/OurVision/1_4.webp"}
-              alt="ACM members attend an intern panel and boba social during the ACM Kickoff Week."
+              src={OurVision4}
+              alt="Image 4"
               fill
-              quality={85}
+              className="object-cover"
             />
           </div>
         </div>
       </div>
       <div className="text-acm-gray-500 flex flex-col items-center text-center">
-        <h1 className="w-auto text-4xl font-bold">OUR VISION</h1>
+        <p className="w-auto text-4xl font-bold">OUR VISION</p>
         <p className="mt-8 w-4/5 text-3xl leading-normal font-medium">
           Our Vision is simple: We want to improve our members professionally,
           technically, and personally. We want to help our members grow
