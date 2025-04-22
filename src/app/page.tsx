0a55599c7dc597e3f15af75881abf7fa9379d271
Stuart Arief <stuartarief@ucr-secure-20-10-13-128-139.wnet.ucr.edu>
2025-04-21 17:23:50 -0700

    Fixed statsGrid call position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import StatsGrid from "@/components/home/statsgrid";
 import ViewAllLink from "@/components/home/viewalllink";
 import Header from "@/components/home/header";
 import UpcomingEvents from "@/components/home/UpcomingEvents";
 import SeeMoreLink from "@/components/home/seemorelink";
 import FeaturedProjects from "@/components/home/featuredprojects";
+import StatsGrid from "@/components/home/statsgrid";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <StatsGrid />
       <ViewAllLink link="/programs/spark">Spark</ViewAllLink>
       <ViewAllLink link="/programs/create">Create</ViewAllLink>
       <ViewAllLink link="/programs/forge">Forge</ViewAllLink>
-      <StatsGrid />
       <UpcomingEvents />
       <SeeMoreLink />
       <FeaturedProjects />
