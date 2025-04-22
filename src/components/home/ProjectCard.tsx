53cf67e49c0996a8cf30935b7d4e82b92d3334de
lbojo006 <luisbojorquezgoal@gmail.com>
2025-04-22 16:26:48 -0700

    Added Assets and Fixed the Card

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import Image, { StaticImageData } from "next/image";
-import CutOut from "@/public/Home/CutOut.png";
 import Link from "next/link";
 
 interface ProjectCardProps {
@@ -10,32 +9,10 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ project, image, link }: ProjectCardProps) => {
   return (
-    <Link
-      href={link}
-      className="relative my-5 flex overflow-hidden rounded-lg p-4"
-      style={{
-        backgroundImage: `url(${image.src})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="absolute inset-0 backdrop-blur-sm"></div>
-      <div className="bg-acm-gray-100 absolute inset-0 opacity-50 backdrop-blur-sm"></div>
-      <div className="z-10">
-        <Image
-          src={image}
-          alt={`${project} Image`}
-          className="aspect-[20/15] scale-x-130 scale-y-140 lg:p-2"
-        />
-        <Image
-          src={CutOut}
-          alt="CutOut"
-          layout="fill"
-          className="absolute inset-0 scale-y-105"
-        />
-        <div className="z-20 mt-5 w-full translate-y-3 rounded-4xl bg-white p-2 lg:-translate-y-3">
-          {project}
-        </div>
+    <Link href={link}>
+      <Image src={image} alt={`${project} Image`} />
+      <div className="absolute w-100 translate-x-5 -translate-y-20 rounded-3xl bg-white p-3 pl-5 text-2xl font-semibold">
+        {project}
       </div>
     </Link>
   );
--- src/components/home/featuredprojects.tsx
+++ src/components/home/featuredprojects.tsx
@@ -1,46 +1,49 @@
 import Link from "next/link";
 import ProjectCard from "../home/ProjectCard";
-import Spark1 from "@/public/Spark/pad.webp";
-import Spark2 from "@/public/Spark/aviatr.webp";
-import Spark3 from "@/public/Spark/saf.webp";
-import Create1 from "@/public/Create/acmhydra.webp";
-import Create2 from "@/public/Create/witl.webp";
-import Create3 from "@/public/Create/3d.webp";
+import Spark1 from "@/public/Home/Spark/pad.png";
+import Spark2 from "@/public/Home/Spark/aviatr.png";
+import Spark3 from "@/public/Home/Spark/saf.png";
+import Create1 from "@/public/Home/Create/acm.png";
+import Create2 from "@/public/Home/Create/witl.png";
+import Create3 from "@/public/Home/Create/nsu.png";
+import Forge1 from "@/public/Home/Forge/wzchess.png";
+import Forge2 from "@/public/Home/Forge/drone.png";
+import Forge3 from "@/public/Home/Forge/blank.png";
 
 const FeaturedProjects = () => {
   return (
-    <div>
+    <div className="m-10">
       <div className="mt-5 flex items-center justify-center">
         <p className="text-acm-gray-500 text-6xl font-bold">
           FEATURED PROJECTS
         </p>
       </div>
       <div className="text-acm-gray-500 mx-10 mt-10 font-bold">
-        <p className="text-3xl">ACM SPARK</p>
-        <div className="grid grid-cols-3 gap-4 text-lg">
+        <p className="pb-3 text-4xl">ACM SPARK</p>
+        <div className="grid grid-cols-3 gap-8 text-lg">
           <ProjectCard project="PHI ALPHA DELTA" image={Spark1} link="/" />
           <ProjectCard project="AVIAT’R" image={Spark2} link="/" />
           <ProjectCard project="SAF" image={Spark3} link="/" />
         </div>
-        <Link href="/" className="flex justify-end text-lg">
+        <Link href="/" className="mt-3 flex justify-end text-2xl">
           View All Spark Projects →
         </Link>
-        <p className="text-3xl">ACM CREATE</p>
-        <div className="grid grid-cols-3 gap-4 text-lg">
+        <p className="text-4xl">ACM CREATE</p>
+        <div className="grid grid-cols-3 gap-8 text-lg">
           <ProjectCard project="ACM" image={Create1} link="/" />
           <ProjectCard project="WITL" image={Create2} link="/" />
           <ProjectCard project="NSU" image={Create3} link="/" />
         </div>
-        <Link href="/" className="flex justify-end text-lg">
+        <Link href="/" className="mt-3 flex justify-end text-2xl">
           View All Create Projects →
         </Link>
-        <p className="text-3xl">ACM FORGE</p>
-        <div className="grid grid-cols-3 gap-4 text-lg">
-          <ProjectCard project="PHI ALPHA DELTA" image={Spark1} link="/" />
-          <ProjectCard project="AVIAT’R" image={Spark2} link="/" />
-          <ProjectCard project="SAF" image={Spark3} link="/" />
+        <p className="text-4xl">ACM FORGE</p>
+        <div className="grid grid-cols-3 gap-8 text-lg">
+          <ProjectCard project="WIZARD CHESS" image={Forge1} link="/" />
+          <ProjectCard project="DRONES" image={Forge2} link="/" />
+          <ProjectCard project="ARMS" image={Forge3} link="/" />
         </div>
-        <Link href="/" className="mb-5 flex justify-end text-lg">
+        <Link href="/" className="mt-3 mb-5 flex justify-end text-2xl">
           View All Forge Projects →
         </Link>
       </div>
