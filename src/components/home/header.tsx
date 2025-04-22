fd39c5e24a54933dacc7eb0d3bf9ab826be371fe
stanleylew5 <stanley43595@gmail.com>
2025-04-22 16:35:26 -0700

    cleanup

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import ACM_Keys from "@/public/Home/ACM_Keys.svg";
+import Keys from "@/public/Home/keys.svg";
 const Header = () => {
   return (
     <div className="text-acm-gray-500 mt-10 flex flex-col items-center justify-center text-6xl font-semibold">
       <p>ASSOCIATION OF</p>
       <p>COMPUTING MACHINERY</p>
-      <Image src={ACM_Keys} alt="ACM Keys" className="mt-5" />
+      <Image src={Keys} alt="Keys" className="mt-5" />
     </div>
   );
 };
