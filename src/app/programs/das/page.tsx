3f4dc6bff03971426bc120060974e6b2a3eee6db
Sweden <84001329+swedenagu@users.noreply.github.com>
2025-04-21 21:07:15 -0700

    Revert "Join Now text, button, and image. Has a placeholder at the moment. On…"

--- src/app/programs/das/page.tsx
+++ src/app/programs/das/page.tsx
@@ -1,16 +1,9 @@
 import Header from "@/components/programs/das/header";
-import JoinNow from "@/components/programs/join-now";
-import DAS2 from "@/public/DAS_2.jpg";
 
 const Page = () => {
   return (
     <div>
       <Header />
-      <JoinNow
-        textColor="text-white"
-        backgroundColor="bg-[#B889EA]"
-        image={DAS2}
-      />
     </div>
   );
 };
--- src/app/programs/spark/page.tsx
+++ src/app/programs/spark/page.tsx
@@ -1,16 +1,10 @@
 import Header from "@/components/programs/spark/header";
-import JoinNow from "@/components/programs/join-now";
-import SPARK1 from "@/public/spark_1.jpg";
 import ProjectGrid from "@/components/programs/spark/projectgrid";
+
 const Page = () => {
   return (
     <div>
       <Header />
-      <JoinNow
-        textColor="text-acm-gray-500"
-        backgroundColor="bg-amber-100"
-        image={SPARK1}
-      />
       <ProjectGrid />
     </div>
   );
