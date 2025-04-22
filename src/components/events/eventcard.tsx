829f3450ec05c843817fbb247480e87250c115a1
j1munoz <jimmymunoz397@gmail.com>
2025-04-21 19:41:48 -0700

    Run format

--- src/components/events/eventcard.tsx
+++ src/components/events/eventcard.tsx
@@ -23,14 +23,14 @@ const EventCard = ({
 }: EventCardProps) => {
   return (
     <div
-      className={`flex flex-col items-start justify-between w-[18vw] ${bgColor} ${textColor ? textColor : "text-acm-gray-500"} gap-2 rounded-xl py-6 px-4`}
+      className={`flex w-[18vw] flex-col items-start justify-between ${bgColor} ${textColor ? textColor : "text-acm-gray-500"} gap-2 rounded-xl px-4 py-6`}
     >
       <div className="flex flex-col">
         <p className="text-5xl font-bold">{title}</p>
-          <p className="font-normal text-2xl">{date}</p>
-          <p className="font-light text-xl">
-            {location} - {startintgTime}
-          </p>
+        <p className="text-2xl font-normal">{date}</p>
+        <p className="text-xl font-light">
+          {location} - {startintgTime}
+        </p>
       </div>
       <div className="ml-4 flex w-full items-end justify-end">
         <Image src={image} alt="Event Image" />
