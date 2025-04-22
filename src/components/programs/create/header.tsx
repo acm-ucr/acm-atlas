a06905d5fe7c501e8ed113ee0bb66ae69c29b1f8
shuuvi <janeleebmes@gmail.com>
2025-04-22 00:45:24 -0700

    created all of the headers for the programs

--- src/components/programs/create/header.tsx
+++ src/components/programs/create/header.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import Create from "@/public/create.svg";
+
 const Header = () => {
   return (
-    <p className="text-acm-gray-500 mt-10 flex justify-center text-center text-4xl font-bold md:text-5xl">
-      CREATE
-    </p>
+    <div className="text-acm-gray-500 relative p-10 text-2xl font-semibold">
+      <div className="text-center">
+        <p className="flex justify-center p-7 text-5xl font-bold">CREATE</p>
+        <p className="pb-7">UI/UX Design</p>
+      </div>
+      <p className="ml-[15%] w-3/8">
+        ACM Create is a 10-week, quarter-long program focused on developing
+        wireframes for Spark projects using Figma.
+      </p>
+      <Image className="absolute top-15 right-90" src={Create} alt="Spark" />
+    </div>
   );
 };
 
