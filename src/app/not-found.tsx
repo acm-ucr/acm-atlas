51743d98d39b525f14dfa63d3fd6e9260f9ce576
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-21 19:36:51 -0700

    fixed merge

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-<<<<<<< HEAD
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <p className="text-acm-navy flex w-screen items-center justify-center text-4xl font-semibold">
         PAGE NOT FOUND
@@ -13,10 +12,6 @@ const NotFound = () => {
       >
         Go Back Home
       </Link>
-=======
-    <div className="flex min-h-screen items-center justify-center">
-      <p className="text-acm-navy text-4xl font-semibold">PAGE NOT FOUND</p>
->>>>>>> f9ae7244cbaced02750bddbea5ea5a29c4f095cc
     </div>
   );
 };
