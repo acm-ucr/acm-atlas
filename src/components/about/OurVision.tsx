87f80c6fbb86d5338874c586a1506876e3ecf895
Andres Lopez <alope759@ucr.edu>
2025-04-21 18:18:19 -0700

    Corrected formatting and added alt text.

--- src/components/about/OurVision.tsx
+++ src/components/about/OurVision.tsx
@@ -1,30 +1,49 @@
 import Image from "next/image";
-const placeholder = "stanley.webp";
 
 const OurVision = () => {
   return (
     <div className="ml-16 grid grid-cols-2">
-      <div className="grid grid-cols-5">
-        <div className="col-span-2 grid grid-rows-3 gap-4">
+      <div className="grid w-2/3 grid-cols-5">
+        <div className="col-span-2 grid grid-rows-3 gap-6">
           <div className="relative mt-4 aspect-[1/1] w-auto overflow-hidden rounded-xl">
-            <Image src={placeholder} alt="Placeholder Image 1" fill />
+            <Image
+              src={"AboutUs/OurVision/1_1.jpg"}
+              alt="ACM Members attend an in-progress general ACM meeting."
+              fill
+              quality={85}
+            />
           </div>
-          <div className="relative row-span-2 h-[55.5%] w-auto overflow-hidden rounded-xl">
-            <Image src={placeholder} alt="Placeholder Image 2" fill />
+          <div className="relative row-span-2 h-[49.5%] w-auto overflow-hidden rounded-xl">
+            <Image
+              src={"AboutUs/OurVision/1_3.jpg"}
+              alt="A screen presenting ACM VP Jerry Li's LinkedIn is provided to members as the exemplar."
+              fill
+              quality={85}
+            />
           </div>
         </div>
         <div className="col-span-3 m-4 grid grid-rows-2 gap-4">
           <div className="relative aspect-[1/1] w-auto overflow-hidden rounded-xl">
-            <Image src={placeholder} alt="Placeholder Image 3" fill />
+            <Image
+              src={"AboutUs/OurVision/1_2.jpg"}
+              alt="ACM members and UCR students alike attend an ACM workshop on the basics of Python programming at the Pentland Hills Foxhole."
+              fill
+              quality={85}
+            />
           </div>
-          <div className="relative mt-[-48] aspect-[16/9] w-auto overflow-hidden rounded-xl">
-            <Image src={placeholder} alt="Placeholder Image 4" fill />
+          <div className="relative mt-[-48] aspect-[15/9] w-auto overflow-hidden rounded-xl">
+            <Image
+              src={"AboutUs/OurVision/1_4.jpg"}
+              alt="ACM members attend an intern panel and boba social during the ACM Kickoff Week."
+              fill
+              quality={85}
+            />
           </div>
         </div>
       </div>
       <div className="text-acm-gray-500 flex flex-col items-center text-center">
-        <h1 className="w-1/2 text-3xl font-bold">OUR VISION</h1>
-        <p className="mt-12 w-2/3 text-2xl font-medium">
+        <h1 className="w-auto text-4xl font-bold">OUR VISION</h1>
+        <p className="mt-8 w-4/5 text-3xl leading-normal font-medium">
           Our Vision is simple: We want to improve our members professionally,
           technically, and personally. We want to help our members grow
           throughout their college career, whether they enter as a first-year
