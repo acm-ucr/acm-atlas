f30791039e7c9c0cd76cab728a3e368dc526628a
Shaodw709 <amirvs9675@gmail.com>
2025-04-22 20:30:43 -0700

    add svg

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import Keys from "@/public/home/keys.svg";
+import blue from "@/public/home/blueCircle.svg";
+import yellow from "@/public/home/yellowCircle.svg";
 const Landing = () => {
   return (
     <div className="text-acm-gray-500 mt-10 flex flex-col items-center justify-center text-6xl font-semibold">
       <p>ASSOCIATION OF</p>
       <p>COMPUTING MACHINERY</p>
       <Image src={Keys} alt="Keys" className="mt-5" />
+      <div>
+        <Image src={blue} alt="bue" />
+        <Image src={yellow} alt="yellow" />
+      </div>
     </div>
   );
 };
