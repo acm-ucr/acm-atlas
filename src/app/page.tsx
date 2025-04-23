ca429ade53a7e853ccde3dd0cf13daca84d691c5
stanleylew5 <stanley43595@gmail.com>
2025-04-23 16:40:24 -0700

    build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import ViewAllLink from "@/components/home/viewalllink";
-import Header from "@/components/hacks/Header";
 import Upcoming from "@/components/home/upcoming";
 import SeeMoreLink from "@/components/home/seemorelink";
 import FeaturedProjects from "@/components/home/featuredprojects";
@@ -8,7 +7,6 @@ import Landing from "@/components/home/landing";
 const Home = () => {
   return (
     <div>
-      <Header />
       <Landing />
       <ViewAllLink link="/programs/spark">Spark</ViewAllLink>
       <ViewAllLink link="/programs/create">Create</ViewAllLink>
