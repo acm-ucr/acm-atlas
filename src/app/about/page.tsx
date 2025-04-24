3f89e5497d6a7699790656671b925332c4adde38
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-23 20:52:59 -0700

    fixed errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/about/Header";
 import WhyWeDoIt from "@/components/about/whywedoit";
 import { WhyWeDoItData } from "@/data/whywedoit";
 
 const Page = () => {
   return (
     <div>
-      <Header />
       <WhyWeDoIt
         header={WhyWeDoItData.at(0)?.header}
         pText={WhyWeDoItData.at(0)?.pText}
