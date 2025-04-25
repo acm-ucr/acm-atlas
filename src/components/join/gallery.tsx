baafd1d3361afd3e1583269ad98a28022bb6cc05
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-24 19:58:44 -0700

    further developed basic skeleton of grid

--- src/components/join/gallery.tsx
+++ src/components/join/gallery.tsx
@@ -5,17 +5,17 @@ import group from "@/public/join/gallery/group.webp";
 
 const Gallery = () => {
   return (
-    <div className="flex w-screen flex-col items-center">
+    <div className="mx-20 flex flex-col items-center">
       <p className="text-acm-gray-500 my-16 text-3xl font-bold">GALLERY</p>
-      <div className="grid grid-cols-5 gap-8">
+      <div className="grid grid-cols-2 gap-8">
         <div className="grid gap-8">
           <Image
-            className="h-full w-full rounded-3xl object-cover"
+            className="col-span-2 grid h-full w-full rounded-3xl object-cover"
             src={arcades}
             alt="Round1 Bowling Social"
           />
           <Image
-            className="h-full w-full rounded-3xl object-cover"
+            className="col-span-2 grid h-full w-full rounded-3xl object-cover"
             src={claw}
             alt="Round1 Bowling Social"
           />
@@ -26,17 +26,17 @@ const Gallery = () => {
           alt="Group photo at Round1 Bowling social"
         />
         <div className="grid gap-8">
-          <div className="bg-acm-blue-600 h-40 w-40 rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 h-40 w-40 rounded-3xl object-cover"></div>
+          <div className="bg-acm-blue-600 col-span-2 h-60 w-full rounded-3xl object-cover"></div>
+          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
         </div>
         <div className="grid gap-8">
-          <div className="bg-acm-blue-600 h-40 w-40 rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 h-40 w-40 rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 h-40 w-40 rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 h-40 w-40 rounded-3xl object-cover"></div>
+          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
+          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
+          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
+          <div className="bg-acm-blue-600 w-full rounded-3xl col-span-2 h-46 object-cover"></div>
         </div>
       </div>
-      <button className="my-10 bg-acm-blue-700 rounded-xl px-10 py-1 text-2xl text-white">
+      <button className="bg-acm-blue-700 my-10 rounded-xl px-10 py-1 text-2xl text-white">
         See More
       </button>
     </div>
