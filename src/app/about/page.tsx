162c6f62d69d50110fb5490a3d06ca12707f90a6
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-26 13:51:47 -0700

    fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,12 @@
+import AboutUs from "@/components/about/aboutus";
+import OurVision from "@/components/about/ourvision";
 import WhyWeDoIt from "@/components/about/whywedoit";
 
 const Page = () => {
   return (
     <div>
+      <AboutUs />
+      <OurVision />
       <WhyWeDoIt />
     </div>
   );
--- src/components/about/whywedoit.tsx
+++ src/components/about/whywedoit.tsx
@@ -34,10 +34,10 @@ const WhyWeDoIt = () => {
         </div>
       </div>
       <div className="text-center">
-        <p className="text-acm-gray-500 py-5 text-5xl font-bold">
+        <p className="text-acm-gray-500 py-5 text-4xl font-bold">
           WHY WE DO IT
         </p>
-        <p className="text-acm-gray-500 text-3xl leading-10">
+        <p className="text-acm-gray-500 text-2xl leading-10">
           We want UCR to become a premier institution for Computer Science and
           Engineering. University courses provide a good foundation, but we
           strive to build upon that. But more importantly, we want our members
