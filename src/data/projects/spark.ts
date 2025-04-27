026c3abe7f147705abdc806c3ce46e90667898e8
lbojo006 <luisbojorquezgoal@gmail.com>
2025-04-27 15:50:57 -0700

    Added all data to Spark.ts fully working links

--- src/data/projects/spark.ts
+++ src/data/projects/spark.ts
@@ -1,13 +1,73 @@
-import pad from "@/public/programs/spark/pad.webp";
+import aag from "@/public/programs/spark/aag.webp";
+import acmhydra from "@/public/programs/spark/acmhydra.webp";
+import aiatucr from "@/public/programs/spark/aiatucr.webp";
+import ais from "@/public/programs/spark/ais.webp";
+import artfactory from "@/public/programs/spark/artfactory.webp";
+import asme from "@/public/programs/spark/asme.webp";
 import aviatr from "@/public/programs/spark/aviatr.webp";
+import bap from "@/public/programs/spark/bap.webp";
+import csa from "@/public/programs/spark/csa.webp";
+import cse from "@/public/programs/spark/cse.webp";
+import cvdsa from "@/public/programs/spark/cvdsa.webp";
+import ewb from "@/public/programs/spark/ewb.webp";
+import fencing from "@/public/programs/spark/fencing.webp";
+import gardening from "@/public/programs/spark/gardening.webp";
+import h4b from "@/public/programs/spark/h4b.webp";
+import hearts from "@/public/programs/spark/hearts.webp";
+import highlandergloves from "@/public/programs/spark/highlandergloves.webp";
+import hiss from "@/public/programs/spark/hiss.webp";
+import hlg from "@/public/programs/spark/hlg.webp";
 import hsa from "@/public/programs/spark/hsa.webp";
+import hsp from "@/public/programs/spark/hsp.webp";
+import ieee from "@/public/programs/spark/ieee.webp";
+import iranianstudentassociation from "@/public/programs/spark/iranianstudentassociation.webp";
+import isa from "@/public/programs/spark/isa.webp";
+import jnm from "@/public/programs/spark/j&m.webp";
+import leap from "@/public/programs/spark/leap.webp";
+import pad from "@/public/programs/spark/pad.webp";
+import prishi from "@/public/programs/spark/prishi.webp";
+import quant from "@/public/programs/spark/quant.webp";
+import robosub from "@/public/programs/spark/robosub.webp";
+import rtools from "@/public/programs/spark/rtools.webp";
+import saf from "@/public/programs/spark/saf.webp";
+import seniordesign from "@/public/programs/spark/seniordesign.webp";
 
 const spark = [
   {
-    photo: pad,
-    name: "Phi Alpha Delta",
-    github: "https://github.com/acm-ucr/pad-website",
-    website: "https://pad.ucrhighlanders.org/",
+    photo: aag,
+    name: "AAG",
+    github: "https://github.com/acm-ucr/aag-website",
+    website: "https://aag.ucrhighlanders.org/",
+  },
+  {
+    photo: acmhydra,
+    name: "ACM Hydra",
+    github: "https://github.com/acm-ucr/acm-hydra",
+    website: "https://acm.cs.ucr.edu/",
+  },
+  {
+    photo: aiatucr,
+    name: "AI at UCR",
+    github: "https://github.com/acm-ucr/aiatucr-website",
+    website: "https://ai-ucr.ucrhighlanders.org/",
+  },
+  {
+    photo: ais,
+    name: "AIS",
+    github: "https://github.com/acm-ucr/ais-website",
+    website: "https://ais.ucrhighlanders.org/",
+  },
+  {
+    photo: artfactory,
+    name: "Art Factory",
+    github: "https://github.com/acm-ucr/art-factory-website",
+    website: "https://artfactory.ucrhighlanders.org/",
+  },
+  {
+    photo: asme,
+    name: "ASME",
+    github: "https://github.com/acm-ucr/asme-website",
+    website: "https://asme.ucrhighlanders.org/",
   },
   {
     photo: aviatr,
@@ -16,10 +76,118 @@ const spark = [
     website: "https://aviatr.ucrhighlanders.org/",
   },
   {
+    photo: bap,
+    name: "BAP",
+    github: "https://github.com/acm-ucr/bap-website",
+    website: "https://bap.ucrhighlanders.org/",
+  },
+  {
+    photo: csa,
+    name: "CSA",
+    github: "https://github.com/acm-ucr/csa-website",
+    website: "https://csa.ucrhighlanders.org/",
+  },
+  {
+    photo: cse,
+    name: "CSE",
+    github: "https://github.com/acm-ucr/CSE-AI-resources",
+    website: "https://learnai.cs.ucr.edu/",
+  },
+  {
+    photo: cvdsa,
+    name: "CVD Society",
+    github: "https://github.com/acm-ucr/cvdsa-uoft-website",
+    website: "https://acm-ucr.github.io/cvdsa-uoft-website/",
+  },
+  {
+    photo: ewb,
+    name: "Engineers Without Borders",
+    github: "https://github.com/acm-ucr/ewb-website",
+    website: "https://ewb.ucrhighlanders.org/",
+  },
+  {
+    photo: fencing,
+    name: "Fencing Club",
+    github: "https://github.com/acm-ucr/fencing-website",
+    website: "https://fencing.ucrhighlanders.org/",
+  },
+  {
+    photo: gardening,
+    name: "Gardening Club",
+    github: "https://github.com/acm-ucr/gardening-website",
+    website: "https://gardening.ucrhighlanders.org/",
+  },
+  {
+    photo: h4b,
+    name: "Hungry 4 Biscuits",
+    github: "https://github.com/acm-ucr/h4b-website",
+    website: "https://h4b.ucrhighlanders.org/",
+  },
+  {
+    photo: hearts,
+    name: "HEARTS",
+    github: "https://github.com/acm-ucr/ucr-hearts-website",
+    website: "https://hearts.ucrhighlanders.org/",
+  },
+  {
+    photo: highlandergloves,
+    name: "Highlander Gloves",
+    github: "https://github.com/acm-ucr/highlander-gloves-website",
+    website: "https://highlandergloves.ucrhighlanders.org/",
+  },
+  {
+    photo: hiss,
+    name: "HISS",
+    github: "https://github.com/acm-ucr/hiss-website",
+    website: "https://hiss.ucrhighlanders.org/",
+  },
+  {
+    photo: hlg,
+    name: "HLG",
+    github: "https://github.com/acm-ucr/hlg-website",
+    website: "https://hlg.ucrhighlanders.org/",
+  },
+  {
     photo: hsa,
     name: "Hmong Student Association",
     github: "https://github.com/acm-ucr/hsa-website",
     website: "https://hsa.ucrhighlanders.org/",
+  },
+  {
+    photo: hsp,
+    name: "Highlander Space Program",
+    github: "https://github.com/acm-ucr/hsp-website",
+    website: "https://hsp.ucrhighlanders.org/",
+  },
+  {
+    photo: ieee,
+    name: "IEEE",
+    github: "https://github.com/acm-ucr/ieee-website",
+    website: "https://ieee.ucrhighlanders.org/",
+  },
+  {
+    photo: iranianstudentassociation,
+    name: "Iranian Student Association",
+    github: "https://github.com/acm-ucr/iranian-student-association-website",
+    website: "https://iranianstudentassoc.ucrhighlanders.org/",
+  },
+  {
+    photo: isa,
+    name: "ISA",
+    github: "https://github.com/acm-ucr/isa-website",
+    website: "https://isa.ucrhighlanders.org/",
+  },
+  {
+    photo: jnm,
+    name: "J&M",
+    github: "https://github.com/acm-ucr/jm-products-website",
+    website: "https://www.jmproducts.com",
+  },
+  {
+    photo: leap,
+    name: "Leap",
+    github: "https://github.com/acm-ucr/leap-website",
+    website: "https://leap.ucrhighlanders.org/",
   },
   {
     photo: pad,
@@ -28,16 +196,40 @@ const spark = [
     website: "https://pad.ucrhighlanders.org/",
   },
   {
-    photo: aviatr,
-    name: "Aviat'R",
-    github: "https://github.com/acm-ucr/aviatr-website",
-    website: "https://aviatr.ucrhighlanders.org/",
+    photo: prishi,
+    name: "Prishi",
+    github: "https://github.com/acm-ucr/rishi-website",
+    website: "https://prishi.ucrhighlanders.org/",
   },
   {
-    photo: hsa,
-    name: "Hmong Student Association",
-    github: "https://github.com/acm-ucr/hsa-website",
-    website: "https://hsa.ucrhighlanders.org/",
+    photo: quant,
+    name: "Quant",
+    github: "https://github.com/acm-ucr/quant-website",
+    website: "https://quant.ucrhighlanders.org/",
+  },
+  {
+    photo: robosub,
+    name: "Robosub",
+    github: "https://github.com/acm-ucr/robosub-website",
+    website: "https://robosub.ucrhighlanders.org/",
+  },
+  {
+    photo: rtools,
+    name: "RTools",
+    github: "https://github.com/acm-ucr/r-tools",
+    website: "https://rtools.ucrhighlanders.org/",
+  },
+  {
+    photo: saf,
+    name: "SAF",
+    github: "https://github.com/acm-ucr/saf-website",
+    website: "https://saf.ucrhighlanders.org/",
+  },
+  {
+    photo: seniordesign,
+    name: "Senior Design",
+    github: "https://github.com/acm-ucr/senior-design-website",
+    website: "https://seniors.cs.ucr.edu/",
   },
 ];
 
