47de87d4ff5a46d4e393b616c8e671ca71bef592
Vincent Dinh <vincent.dinh401@gmail.com>
2025-04-28 15:58:20 -0700

    Draft for Carousel

--- src/app/programs/spark/page.tsx
+++ src/app/programs/spark/page.tsx
@@ -2,6 +2,21 @@ import Header from "@/components/programs/spark/header";
 import Joinus from "@/components/programs/joinus";
 import Spark1 from "@/public/spark/spark1.webp";
 import ProjectGrid from "@/components/programs/spark/projectgrid";
+import spark from "@/data/teckstacks/spark";
+import {
+  SiEslint,
+  SiNextdotjs,
+  SiTypescript,
+  SiTailwindcss,
+} from "react-icons/si";
+import Carousel from "@/components/carousel";
+
+const icons = {
+  SiEslint,
+  SiNextdotjs,
+  SiTypescript,
+  SiTailwindcss,
+};
 
 const Page = () => {
   return (
@@ -12,6 +27,7 @@ const Page = () => {
         backgroundColor="bg-acm-yellow-200"
         image={Spark1}
       />
+      <Carousel data={spark} icons={icons} />
       <ProjectGrid />
     </div>
   );
---
+++ src/data/teckstacks/spark.ts
@@ -0,0 +1,27 @@
+const spark = [
+  {
+    name: "ESLint",
+    icon: "SiEslint",
+    borderColor: "border-[color:var(--color-acm-eslint)]",
+    iconColor: "text-[color:var(--color-acm-eslint)]",
+  },
+  {
+    name: "Next.js",
+    icon: "SiNextdotjs",
+    borderColor: "border-black",
+    iconColor: "text-black",
+  },
+  {
+    name: "TypeScript",
+    icon: "SiTypescript",
+    borderColor: "border-[color:var(--color-acm-typescript)]",
+    iconColor: "text-[color:var(--color-acm-typescript)]",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "SiTailwindcss",
+    borderColor: "border-[color:var(--color-acm-tailwindcss)]",
+    iconColor: "text-[color:var(--color-acm-tailwindcss)]",
+  },
+];
+export default spark;
