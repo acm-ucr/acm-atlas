546a4b815f5272b78d68b80efe1234d0bb30b4cd
Yoshinao <yoshinaoshimono@gmail.com>
2025-04-27 19:53:53 -0700

    imported images instead of passing links

--- src/data/opportunities.ts
+++ src/data/opportunities.ts
@@ -1,35 +1,41 @@
+import spark from "@/public/Home/CurrentOpportunities/spark.svg"
+import forge from "@/public/Home/CurrentOpportunities/forge.svg"
+import create from "@/public/Home/CurrentOpportunities/create.svg"
+import das from "@/public/Home/CurrentOpportunities/das.svg"
+import bitbyte from "@/public/Home/CurrentOpportunities/bitbyte.svg"
+
 const opportunites = [
   {
     name: "spark",
-    card_background: "Home/CurrentOpportunities/spark.svg",
+    card_background: spark,
     year_and_quarter: "SPRING '24",
     link: "/programs/spark",
     application_link: "/"
   },
   {
     name: "forge",
-    card_background: "Home/CurrentOpportunities/forge.svg",
+    card_background: forge,
     year_and_quarter: "SPRING '24",
     link: "/programs/forge",
     application_link: "/"
   },
   {
     name: "create",
-    card_background: "Home/CurrentOpportunities/create.svg",
+    card_background: create,
     year_and_quarter: "SPRING '24",
     link: "/programs/create",
     application_link: "/"
   },
   {
     name: "das",
-    card_background: "Home/CurrentOpportunities/das.svg",
+    card_background: das,
     year_and_quarter: "SPRING '24",
     link: "/programs/das",
     application_link: "/"
   },
   {
     name: "bit/byte",
-    card_background: "Home/CurrentOpportunities/bitbyte.svg",
+    card_background: bitbyte,
     year_and_quarter: "SPRING '24",
     link: "/programs/bitbyte",
     application_link: "/"
