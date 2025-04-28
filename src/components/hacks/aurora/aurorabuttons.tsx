12a529d56a3ecfbc6f2762dd2ab6ca349f2a6635
arkbot51 <rhahee528@gmail.com>
2025-04-28 12:54:14 -0700

    made fonts smaller to match header

--- src/components/hacks/aurora/aurorabuttons.tsx
+++ src/components/hacks/aurora/aurorabuttons.tsx
@@ -4,37 +4,37 @@ const Aurorabuttons = () => {
   return (
     <div className="mx-auto grid w-9/12 grid-cols-2 grid-rows-2 gap-y-20">
       <div className="mx-auto flex w-11/12 flex-col justify-center">
-        <p className="text-acm-gray-500 pb-5 text-center text-4xl font-semibold">
+        <p className="text-acm-gray-500 pb-5 text-center text-3xl font-semibold">
           WANT TO GET INVOLVED?
         </p>
         <Link
           href="https://discord.gg/5xzE2qTEKZ"
           target="_blank"
-          className="bg-acm-green-300 flex justify-center rounded-xl px-30 py-7 text-3xl font-medium text-white"
+          className="bg-acm-green-300 flex justify-center rounded-xl px-30 py-7 text-2xl font-medium text-white"
         >
           Join our Discord!
         </Link>
       </div>
       <div className="mx-auto flex w-11/12 flex-col justify-center">
-        <p className="text-acm-gray-500 pb-5 text-center text-4xl font-semibold">
+        <p className="text-acm-gray-500 pb-5 text-center text-3xl font-semibold">
           CHECK OUT OUR BLOGS!
         </p>
         <Link
           href="https://medium.com/@contact.acmucr"
           target="_blank"
-          className="bg-acm-green-300 flex justify-center rounded-xl px-30 py-7 text-3xl font-medium text-white"
+          className="bg-acm-green-300 flex justify-center rounded-xl px-30 py-7 text-2xl font-medium text-white"
         >
           Medium
         </Link>
       </div>
       <div className="mx-auto flex w-11/12 flex-col justify-center">
-        <p className="text-acm-gray-500 pb-5 text-center text-4xl font-semibold">
+        <p className="text-acm-gray-500 pb-5 text-center text-3xl font-semibold">
           CHECK OUT OUR CODE!
         </p>
         <Link
           href="https://github.com/acm-ucr/aurora"
           target="_blank"
-          className="bg-acm-green-300 flex justify-center rounded-xl px-30 py-7 text-3xl font-medium text-white"
+          className="bg-acm-green-300 flex justify-center rounded-xl px-30 py-7 text-2xl font-medium text-white"
         >
           Github
         </Link>
--- src/components/hacks/aurora/stats.tsx
+++ src/components/hacks/aurora/stats.tsx
@@ -5,27 +5,27 @@ const Stats = () => {
     <div className="flex flex-col justify-center py-30">
       <div className="text-acm-gray-500 grid translate-x-[15%] [grid-template-columns:280px_250px_400px] gap-10 pb-18">
         <div className="border-acm-gray-100 flex flex-col justify-center border-r-2">
-          <p className="pb-4 text-7xl font-bold">4000+</p>
-          <p className="text-3xl font-medium">commits</p>
+          <p className="pb-4 text-6xl font-bold">4000+</p>
+          <p className="text-2xl font-medium">commits</p>
         </div>
         <div className="border-acm-gray-100 flex flex-col justify-center border-r-2">
-          <p className="pb-4 text-7xl font-bold">1000+</p>
-          <p className="text-3xl font-medium">PRs</p>
+          <p className="pb-4 text-6xl font-bold">1000+</p>
+          <p className="text-2xl font-medium">PRs</p>
         </div>
         <div className="flex flex-col justify-center">
-          <p className="pb-4 text-7xl font-bold">950+</p>
-          <p className="text-3xl font-medium">issues</p>
+          <p className="pb-4 text-6xl font-bold">950+</p>
+          <p className="text-2xl font-medium">issues</p>
         </div>
       </div>
       <div className="text-acm-gray-500 grid translate-x-[15%] [grid-template-columns:210px_175px_350px] gap-10 pb-10">
         <div className="border-acm-gray-100 flex flex-col justify-center border-r-2">
-          <p className="pb-4 text-7xl font-bold">73</p>
-          <p className="text-3xl font-medium">contributors</p>
+          <p className="pb-4 text-6xl font-bold">73</p>
+          <p className="text-2xl font-medium">contributors</p>
         </div>
         <div className="border-acm-gray-100 flex flex-col justify-center border-r-2 pr-20">
-          <p className="pb-4 text-7xl font-bold">27</p>
+          <p className="pb-4 text-6xl font-bold">27</p>
           <div className="flex">
-            <p className="pr-5 text-3xl font-medium">stars</p>
+            <p className="pr-5 text-2xl font-medium">stars</p>
             <Image
               src="/aurora/aurorastar.webp"
               alt="Star"
@@ -35,8 +35,8 @@ const Stats = () => {
           </div>
         </div>
         <div className="flex flex-col justify-center">
-          <p className="pb-4 text-7xl font-bold">623</p>
-          <p className="text-3xl font-medium">days since we began</p>
+          <p className="pb-4 text-6xl font-bold">623</p>
+          <p className="text-2xl font-medium">days since we began</p>
         </div>
       </div>
     </div>
