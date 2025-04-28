72c2abeccc7f54f12a29a5568778157e5b4466bb
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-27 21:08:36 -0700

    prototype of gallery

--- src/components/join/gallery.tsx
+++ src/components/join/gallery.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
-import arcades from "@/public/join/gallery/arcades.webp";
-import claw from "@/public/join/gallery/claw.webp";
-import group from "@/public/join/gallery/group.webp";
+import join1 from "@/public/join/join1.webp";
+import join2 from "@/public/join/join2.webp";
+import join3 from "@/public/join/join3.webp";
+import join4 from "@/public/join/join4.webp";
+import join5 from "@/public/join/join5.webp";
+import join6 from "@/public/join/join6.webp";
+import join7 from "@/public/join/join7.webp";
+import join8 from "@/public/join/join8.webp";
+import join9 from "@/public/join/join9.webp";
+import join10 from "@/public/join/join10.webp";
+import join11 from "@/public/join/join11.webp";
+import join12 from "@/public/join/join12.webp";
+import join13 from "@/public/join/join13.webp";
+import join14 from "@/public/join/join14.webp";
 
 const Gallery = () => {
   return (
@@ -10,30 +21,81 @@ const Gallery = () => {
       <div className="grid grid-cols-2 gap-8">
         <div className="grid gap-8">
           <Image
-            className="col-span-2 grid h-full w-full rounded-3xl object-cover"
-            src={arcades}
+            className="col-span-2 row-span-4 grid h-full w-full rounded-3xl object-cover"
+            src={join2}
             alt="Round1 Bowling Social"
           />
           <Image
-            className="col-span-2 grid h-full w-full rounded-3xl object-cover"
-            src={claw}
+            className="col-span-2 row-span-1 grid h-full w-full rounded-3xl object-cover"
+            src={join4}
             alt="Round1 Bowling Social"
           />
         </div>
-        <Image
-          className="h-full w-full rounded-3xl object-cover"
-          src={group}
-          alt="Group photo at Round1 Bowling social"
-        />
         <div className="grid gap-8">
-          <div className="bg-acm-blue-600 col-span-2 h-60 w-full rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join11}
+            alt="Group photo at Round1 Bowling social"
+          />
         </div>
         <div className="grid gap-8">
-          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 col-span-2 h-46 w-full rounded-3xl object-cover"></div>
-          <div className="bg-acm-blue-600 w-full rounded-3xl col-span-2 h-46 object-cover"></div>
+          <Image
+            className="col-span-2 row-span-1 h-full w-full rounded-3xl object-cover"
+            src={join1}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="col-span-2 h-full w-full rounded-3xl object-cover"
+            src={join13}
+            alt="Group photo at Round1 Bowling social"
+          />
+        </div>
+        <div className="grid gap-8">
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join6}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join7}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join8}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join9}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join10}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join3}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join12}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join5}
+            alt="Group photo at Round1 Bowling social"
+          />
+          <Image
+            className="h-full w-full rounded-3xl object-cover"
+            src={join14}
+            alt="Group photo at Round1 Bowling social"
+          />
         </div>
       </div>
       <button className="bg-acm-blue-700 my-10 rounded-xl px-10 py-1 text-2xl text-white">
