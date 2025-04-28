8ee80ac7bf356495e50e413adc285dd19f93a927
Yoshinao Shimono <83672225+YosShimono@users.noreply.github.com>
2025-04-27 19:15:02 -0700

    Update src/components/home/currentopportunities.tsx
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>
    Signed-off-by: Yoshinao Shimono <83672225+YosShimono@users.noreply.github.com>

--- src/components/home/currentopportunities.tsx
+++ src/components/home/currentopportunities.tsx
@@ -6,7 +6,7 @@ const CurrentOpportunities = () => {
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <p className="text-6xl font-bold text-gray-600">CURRENT OPPORTUNITIES</p>
       <div className="flex w-[80%] flex-wrap justify-center gap-15">
-        {CurrOppData.map((item, id) => (
+        {opportunities.map((item, id) => (
           <CurrOppCard
             key={id}
             name={item.name}
