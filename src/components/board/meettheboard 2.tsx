6078478d7c01c820803032ba124a1642ee7b845c
stanleylew5 <stanley43595@gmail.com>
2025-04-29 15:45:25 -0700

    delete

--- src/components/board/meettheboard 2.tsx
+++
@@ -1,9 +0,0 @@
-const MeetTheBoard = () => {
-  return (
-    <div className="flex w-screen items-center justify-center">
-      <p className="text-acm-gray-500 text-5xl font-bold">MEET THE BOARD</p>
-    </div>
-  );
-};
-
-export default MeetTheBoard;
