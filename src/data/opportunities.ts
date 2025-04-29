6d6e6efebe86f7866afca7cff53caebb7fe6c455
stanleylew5 <stanley43595@gmail.com>
2025-04-29 16:23:21 -0700

    compile error? hello?

--- src/data/opportunities.ts
+++ src/data/opportunities.ts
@@ -1,41 +1,41 @@
-import spark from "@/public/home/opportunities/spark.svg";
-import forge from "@/public/home/opportunities/forge.svg";
-import create from "@/public/home/opportunities/create.svg";
-import das from "@/public/home/opportunities/das.svg";
-import bitbyte from "@/public/home/opportunities/bitbyte.svg";
+import Spark from "@/public/home/opportunities/spark.svg";
+import Forge from "@/public/home/opportunities/forge.svg";
+import Create from "@/public/home/opportunities/create.svg";
+import DAS from "@/public/home/opportunities/das.svg";
+import BitByte from "@/public/home/opportunities/bitbyte.svg";
 
 const opportunites = [
   {
     name: "spark",
-    card_background: spark,
+    card_background: Spark,
     year_and_quarter: "SPRING '24",
     link: "/programs/spark",
     application_link: "/",
   },
   {
     name: "forge",
-    card_background: forge,
+    card_background: Forge,
     year_and_quarter: "SPRING '24",
     link: "/programs/forge",
     application_link: "/",
   },
   {
     name: "create",
-    card_background: create,
+    card_background: Create,
     year_and_quarter: "SPRING '24",
     link: "/programs/create",
     application_link: "/",
   },
   {
     name: "das",
-    card_background: das,
+    card_background: DAS,
     year_and_quarter: "SPRING '24",
     link: "/programs/das",
     application_link: "/",
   },
   {
     name: "bit/byte",
-    card_background: bitbyte,
+    card_background: BitByte,
     year_and_quarter: "SPRING '24",
     link: "/programs/bitbyte",
     application_link: "/",
