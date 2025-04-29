c40e640429fd3c2a86f6a827eac912119a60dac0
stanleylew5 <stanley43595@gmail.com>
2025-04-29 16:17:39 -0700

    compress photos

--- src/components/join/gallery.tsx
+++ src/components/join/gallery.tsx
@@ -3,104 +3,80 @@ import join1 from "@/public/join/join1.webp";
 import join2 from "@/public/join/join2.webp";
 import join3 from "@/public/join/join3.webp";
 import join4 from "@/public/join/join4.webp";
-import join5 from "@/public/join/join5.webp";
-import join6 from "@/public/join/join6.webp";
-import join7 from "@/public/join/join7.webp";
-import join8 from "@/public/join/join8.webp";
 import join9 from "@/public/join/join9.webp";
 import join10 from "@/public/join/join10.webp";
 import join11 from "@/public/join/join11.webp";
-import join12 from "@/public/join/join12.webp";
-import join13 from "@/public/join/join13.webp";
-import join14 from "@/public/join/join14.webp";
+import join15 from "@/public/join/join15.webp";
+import join16 from "@/public/join/join16.webp";
+import Link from "next/link";
 
 const Gallery = () => {
   return (
-    <div className="mx-20 flex flex-col items-center">
-      <p className="text-acm-gray-500 my-16 text-3xl font-bold">GALLERY</p>
-      <div className="grid grid-cols-2 gap-8">
-        <div className="grid gap-8">
+    <div className="mx-auto flex w-3/4 flex-col items-center">
+      <p className="text-acm-gray-500 mb-8 text-4xl font-bold">GALLERY</p>
+      <div className="grid grid-cols-5 gap-8">
+        <div className="col-span-2 grid grid-rows-2 gap-8">
           <Image
-            className="col-span-2 row-span-4 grid h-full w-full rounded-3xl object-cover"
+            className="grid h-full w-full rounded-3xl object-cover"
             src={join2}
-            alt="Round1 Bowling Social"
+            alt="Beach"
           />
           <Image
-            className="col-span-2 row-span-1 grid h-full w-full rounded-3xl object-cover"
+            className="grid h-full w-full rounded-3xl object-cover"
             src={join4}
-            alt="Round1 Bowling Social"
+            alt="Round1Group"
           />
         </div>
-        <div className="grid gap-8">
+        <Image
+          className="col-span-3 h-full w-full rounded-3xl object-cover"
+          src={join11}
+          alt="ClawMachines"
+        />
+      </div>
+      <div className="mt-8 grid grid-cols-5 gap-8">
+        <Image
+          className="col-span-3 row-span-1 h-full w-full rounded-3xl object-cover"
+          src={join15}
+          alt="Ice Skating"
+        />
+        <div className="col-span-2 grid grid-rows-2 gap-8">
           <Image
             className="h-full w-full rounded-3xl object-cover"
-            src={join11}
-            alt="Group photo at Round1 Bowling social"
+            src={join16}
+            alt="Ice Skating"
           />
-        </div>
-        <div className="grid gap-8">
           <Image
-            className="col-span-2 row-span-1 h-full w-full rounded-3xl object-cover"
+            className="h-full w-full rounded-3xl object-cover"
             src={join1}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="col-span-2 h-full w-full rounded-3xl object-cover"
-            src={join13}
-            alt="Group photo at Round1 Bowling social"
-          />
-        </div>
-        <div className="grid gap-8">
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join6}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join7}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join8}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join9}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join10}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join3}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join12}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join5}
-            alt="Group photo at Round1 Bowling social"
-          />
-          <Image
-            className="h-full w-full rounded-3xl object-cover"
-            src={join14}
-            alt="Group photo at Round1 Bowling social"
+            alt="Beach"
           />
         </div>
       </div>
-      <button className="bg-acm-blue-700 my-10 rounded-xl px-10 py-1 text-2xl text-white">
+      <div className="mt-8 grid grid-cols-5 gap-8">
+        <div className="col-span-2 grid grid-rows-2 gap-8">
+          <Image
+            className="grid h-full w-full rounded-3xl object-cover"
+            src={join9}
+            alt="Round1 Bowling Social"
+          />
+          <Image
+            className="grid h-full w-full rounded-3xl object-cover"
+            src={join10}
+            alt="Round1 Bowling Social"
+          />
+        </div>
+        <Image
+          className="col-span-3 h-full w-full rounded-3xl object-cover"
+          src={join3}
+          alt="Group photo at Round1 Bowling social"
+        />
+      </div>
+      <Link
+        href="/"
+        className="bg-acm-blue-700 my-10 rounded-xl px-10 py-1 text-2xl text-white"
+      >
         See More
-      </button>
+      </Link>
     </div>
   );
 };
