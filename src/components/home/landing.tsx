e3fb5d6be0bfb09a93075925f79c4ad8e37da356
Shaodw709 <amirvs9675@gmail.com>
2025-05-01 23:06:34 -0700

    switch to div

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -1,13 +1,8 @@
 import Image from "next/image";
 import Keys from "@/public/home/keys.svg";
-import blue from "@/public/home/blueCircle.svg";
-import yellow from "@/public/home/yellowCircle.svg";
-// import middle from "@/public/home/middle.svg";
-import green from "@/public/home/green.svg";
-import rightEnd from "@/public/home/rightEnd.svg";
-import top from "@/public/home/top.svg";
+
 import logo from "@/public/home/acmLogo.svg";
-// import dot from "@/public/home/dot.svg";
+
 const Landing = () => {
   return (
     <div className="text-acm-gray-500 mt-10 flex flex-col items-center justify-center text-6xl font-semibold">
@@ -26,46 +21,6 @@ const Landing = () => {
         </p>
 
         <Image src={logo} alt="logo" className="" />
-        <Image
-          src={top}
-          alt="top"
-          className="absolute inset-0 top-[54%] left-[15%]"
-        />
-        <div className="w-[30%]">
-          <Image
-            src={rightEnd}
-            alt="blue"
-            className="absolute inset-0 top-[18.5%] left-[51.8%]"
-          />
-        </div>
-
-        <Image
-          src={blue}
-          alt="blue"
-          className="rounded-b-1/2 absolute inset-0 top-1/3 -left-16 mt-3 w-[40%]"
-        />
-        <Image
-          src={yellow}
-          alt="yellow"
-          className="absolute inset-0 top-[67%] left-0 mt-2"
-        />
-        {/* absolute and z-index don't work together :(
-       <Image
-          src={middle}
-          alt="yellow"
-          className="absolute inset-0 top-[26%] left-[20%] z-20"
-        />
-              <Image
-          src={dot}
-          alt="dot
-          className="absolute top-[100%] left-0 w-10"
-        />
-        */}
-        <Image
-          src={green}
-          alt="green"
-          className="absolute inset-0 top-[40%] left-3/4 w-1/4"
-        />
       </div>
     </div>
   );
