b29bd1758bfaf2bc152a457d91dba187347c71fe
Shaodw709 <amirvs9675@gmail.com>
2025-05-02 17:02:00 -0700

    fixes

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -8,8 +8,8 @@ const Landing = () => {
     <div className="text-acm-gray-500 relative mt-10 flex flex-col items-center justify-center text-6xl font-semibold">
       <p>ASSOCIATION OF</p>
       <p>COMPUTING MACHINERY</p>
-      <Image src={Keys} alt="Keys" className="mt-5" />
-      <div className="z-999 mt-16 flex w-1/2 grid-cols-2 items-center justify-items-center -space-x-4 rounded-2xl bg-transparent/40 px-20 py-25 opacity-100 backdrop-blur-2xl">
+      <Image src={Keys} alt="Keys" className="my-16" />
+      <div className="z-999 mt-16 mb-10 flex h-[600px] w-[1000px] items-center justify-items-center -space-x-4 rounded-2xl bg-transparent/40 px-20 opacity-100 backdrop-blur-3xl">
         <div className="absolute top-[6%] left-[3%] z-24 h-[25px] w-[25px] rounded-full bg-[#F6F6F6]"></div>
         <div className="absolute top-[6%] left-[8%] z-24 h-[25px] w-[25px] rounded-full bg-[#F6F6F6]"></div>
         <div className="absolute top-[6%] left-[13%] z-24 h-[25px] w-[25px] rounded-full bg-[#F6F6F6]"></div>
@@ -20,8 +20,7 @@ const Landing = () => {
             className="absolute top-0 left-0 rounded-t-2xl"
           />
         </div>
-        //
-        <p className="text-acm-gray-500 relative text-xl">
+        <p className="text-acm-gray-500 relative text-3xl">
           ACM at UCR is the largest student-run technical organization with a
           focus on Computer Science. Our goal is to help our members develop
           their technical and professional skills to help them in school and the
@@ -29,16 +28,21 @@ const Landing = () => {
           workshops, professional events, and many more opportunities for our
           members to excel.
         </p>
-        <Image src={logo} alt="logo" className="" />
+        <Image
+          src={logo}
+          alt="logo"
+          className="ml-10 flex h-[250px] w-[250px]"
+        />
       </div>
 
-      <div className="bg-acm-yellow-200 absolute top-[6%5] right-[75%] z-24 h-[300px] w-[350px] rounded-full opacity-90 blur-3xl"></div>
-      <div className="bg-acm-yellow-200 absolute top-[65%] right-[75%] z-24 h-[300px] w-[400px] rounded-full opacity-90 blur-3xl"></div>
-      <div className="bg-acm-blue-700 absolute top-[45%] right-[75%] z-24 h-[300px] w-[400px] rounded-full opacity-70 blur-3xl"></div>
-      <div className="bg-acm-blue-700 absolute top-[63%] left-[75%] z-24 h-[300px] w-[400px] rounded-full opacity-70 blur-3xl"></div>
-      <div className="bg-acm-blue-700 absolute top-[40%] left-[75%] z-24 h-[300px] w-[400px] rounded-full opacity-70 blur-3xl"></div>
+      <div className="bg-acm-yellow-200 absolute top-[65%] right-[78%] z-24 h-[300px] w-[350px] rounded-full opacity-90 blur-3xl"></div>
+      <div className="bg-acm-yellow-200 absolute top-[65%] right-[78%] z-24 h-[300px] w-[400px] rounded-full opacity-90 blur-3xl"></div>
+      <div className="bg-acm-yellow-200 absolute top-[75%] right-[78%] z-24 h-[300px] w-[400px] rounded-full opacity-90 blur-3xl"></div>
+      <div className="bg-acm-blue-700 absolute top-[45%] right-[76%] z-24 h-[300px] w-[400px] rounded-full opacity-70 blur-3xl"></div>
+      <div className="bg-acm-blue-700 absolute top-[69%] left-[76%] z-24 h-[300px] w-[400px] rounded-full opacity-70 blur-3xl"></div>
+      <div className="bg-acm-blue-700 absolute top-[45%] left-[76%] z-24 h-[300px] w-[400px] rounded-full opacity-70 blur-3xl"></div>
       <div className="bg-acm-blue-700 absolute top-[25%] left-[30%] z-24 h-[300px] w-[500px] rounded-full opacity-50 blur-3xl"></div>
-      <div className="bg-acm-blue-700 absolute top-[25%] left-[5%] z-24 h-[300px] w-[500px] rounded-full opacity-50 blur-3xl"></div>
+      <div className="bg-acm-blue-700 absolute top-[25%] left-0 z-24 h-[300px] w-[500px] rounded-full opacity-50 blur-3xl"></div>
       <div className="bg-acm-green-100 absolute top-[25%] left-[50%] z-24 h-[300px] w-[400px] rounded-full opacity-50 blur-3xl"></div>
       <div className="bg-acm-green-100 absolute top-[25%] left-[70%] z-24 h-[300px] w-[400px] rounded-full opacity-50 blur-3xl"></div>
     </div>
