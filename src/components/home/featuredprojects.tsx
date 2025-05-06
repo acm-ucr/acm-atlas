25715a2bbcbfec0c3ab12c062bd6721c97c0d358
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-06 12:01:29 -0700

    mobile responsiveness: featured projects

--- src/components/home/featuredprojects.tsx
+++ src/components/home/featuredprojects.tsx
@@ -13,42 +13,45 @@ import Forge3 from "@/public/forge/blank.webp";
 const FeaturedProjects = () => {
   return (
     <div className="mx-auto w-11/12">
-      <div className="mt-[6vh] flex items-center justify-center">
-        <p className="text-6xl font-bold text-acm-gray-500">
+      <div className="mt-5 flex items-center justify-center">
+        <p className="text-center text-5xl font-bold text-acm-gray-500 md:text-6xl">
           FEATURED PROJECTS
         </p>
       </div>
       <div className="mx-10 mt-10 font-bold text-acm-gray-500">
         <p className="pb-3 text-4xl">ACM SPARK</p>
-        <div className="grid grid-cols-3 gap-8 text-lg">
+        <div className="grid grid-cols-1 gap-8 text-lg md:grid-cols-3">
           <ProjectCard project="PHI ALPHA DELTA" image={Spark1} link="/" />
           <ProjectCard project="AVIAT’R" image={Spark2} link="/" />
           <ProjectCard project="SAF" image={Spark3} link="/" />
         </div>
-        <Link href="/programs/spark" className="mt-3 flex justify-end text-2xl">
+        <Link
+          href="/"
+          className="justify-baseline mb-8 mt-2 flex text-lg md:justify-end md:text-2xl"
+        >
           View All Spark Projects →
         </Link>
         <p className="text-4xl">ACM CREATE</p>
-        <div className="grid grid-cols-3 gap-8 text-lg">
+        <div className="grid grid-cols-1 gap-8 text-lg md:grid-cols-3">
           <ProjectCard project="ACM" image={Create1} link="/" />
           <ProjectCard project="WITL" image={Create2} link="/" />
           <ProjectCard project="NSU" image={Create3} link="/" />
         </div>
         <Link
-          href="/programs/create"
-          className="mt-3 flex justify-end text-2xl"
+          href="/"
+          className="justify-baseline mb-8 mt-2 flex text-lg md:justify-end md:text-2xl"
         >
           View All Create Projects →
         </Link>
         <p className="text-4xl">ACM FORGE</p>
-        <div className="grid grid-cols-3 gap-8 text-lg">
+        <div className="grid grid-cols-1 gap-8 text-lg md:grid-cols-3">
           <ProjectCard project="WIZARD CHESS" image={Forge1} link="/" />
           <ProjectCard project="DRONES" image={Forge2} link="/" />
           <ProjectCard project="ARMS" image={Forge3} link="/" />
         </div>
         <Link
-          href="/programs/forge"
-          className="mb-5 mt-3 flex justify-end text-2xl"
+          href="/"
+          className="justify-baseline mb-8 mt-2 flex text-lg md:justify-end md:text-2xl"
         >
           View All Forge Projects →
         </Link>
