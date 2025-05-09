20b2169879f217e2d645b9b971733c16105ebb16
Andres Lopez <alope759@ucr.edu>
2025-05-08 20:13:29 -0700

    Andres Lopez / Created Used In Component

--- src/app/hacks/aurora/page.tsx
+++ src/app/hacks/aurora/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/hacks/aurora/header";
-import UsedIn from "@/components/hacks/aurora/usedin"
+import UsedIn from "@/components/hacks/aurora/usedin";
 
 const Page = () => {
   return (
