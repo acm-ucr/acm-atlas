c640b4df2734f1b6e0e62f511671daa0475ba2df
arkbot51 <rhahee528@gmail.com>
2025-05-10 12:26:18 -0700

    made programs project cards mobile responsive

--- src/components/programs/projectcard.tsx
+++ src/components/programs/projectcard.tsx
@@ -12,26 +12,36 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ photo, name, github, website }: ProjectCardProps) => {
   return (
-    <div className="flex w-full flex-col rounded-3xl border-4 border-white p-4 shadow-2xl">
+    <div className="flex w-[29vw] flex-col justify-center rounded-[2vw] border-4 border-white px-[1.6vw] py-[2vw] shadow-2xl">
       <div className="relative aspect-[16/9] w-full">
         <Image
           src={photo}
           alt={name}
           fill
-          className="rounded-3xl object-cover"
+          className="rounded-[2vw] object-cover"
         />
       </div>
 
       <div className="mt-4 flex items-center justify-between text-left">
-        <div className="text-2xl font-light">{name}</div>
-        <div className="flex items-center gap-2 text-2xl">
+        <div className="w-[19vw] truncate text-[2.3vw] font-light">{name}</div>
+        <div className="flex items-center gap-2 text-[1.6vw]">
           {github && (
-            <Link href={github} target="_blank" rel="noopener noreferrer">
+            <Link
+              href={github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[2.2vw]"
+            >
               <FaGithub />
             </Link>
           )}
           {website && (
-            <Link href={website} target="_blank" rel="noopener noreferrer">
+            <Link
+              href={website}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[2.2vw]"
+            >
               <FaExternalLinkAlt />
             </Link>
           )}
