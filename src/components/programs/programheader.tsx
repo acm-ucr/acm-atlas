061e751ae0ea42718591428fa05f725ffbfef63c
lbojo006 <luisbojorquezgoal@gmail.com>
2025-05-12 00:50:29 -0700

    Fixed Centered Issue

--- src/components/programs/programheader.tsx
+++ src/components/programs/programheader.tsx
@@ -23,11 +23,16 @@ const ProgramHeader = ({
         <p className="flex justify-center p-5 text-6xl font-bold">{header}</p>
         <p className="pb-5">{subtitle}</p>
       </div>
-      <p className="ml-[8%] mt-60 w-10/12 text-center leading-relaxed sm:ml-[15%] sm:mt-0 sm:w-5/12 sm:pt-0 sm:text-left">
+      <p className="ml-[8%] mt-56 w-10/12 text-center leading-relaxed sm:ml-[15%] sm:mt-0 sm:w-5/12 sm:pt-0 sm:text-left">
         {text}
       </p>
       <Image
-        className="absolute right-[26%] top-[28%] sm:right-[10%] sm:top-[40%] lg:right-[20%] lg:top-[30%]"
+        className="absolute right-[26%] top-[28%] hidden sm:right-[10%] sm:top-[40%] sm:block lg:right-[20%] lg:top-[30%]"
+        src={logo}
+        alt="logo"
+      />
+      <Image
+        className="absolute right-[26%] top-[28%] w-1/2 -translate-y-5 scale-75 sm:hidden"
         src={logo}
         alt="logo"
       />
