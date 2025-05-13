df622d0327c26c4ea0668d66c0c3a6ef35fda34e
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-12 18:48:30 -0700

    ran npm format

--- src/components/home/projectcard.tsx
+++ src/components/home/projectcard.tsx
@@ -11,7 +11,7 @@ const ProjectCard = ({ project, image, link }: ProjectCardProps) => {
   return (
     <Link href={link} className="relative flex justify-center">
       <Image src={image} alt={`${project} Image`} />
-      <div className="absolute bottom-[2.5vh] md:bottom-[2vh]lg:bottom-[1.7vh] w-11/12 rounded-3xl bg-white p-3 pl-5 text-xl md:text-2xl font-semibold">
+      <div className="md:bottom-[2vh]lg:bottom-[1.7vh] absolute bottom-[2.5vh] w-11/12 rounded-3xl bg-white p-3 pl-5 text-xl font-semibold md:text-2xl">
         {project}
       </div>
     </Link>
