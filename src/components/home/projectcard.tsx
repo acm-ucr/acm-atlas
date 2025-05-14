0c25357a41238717f2c108e16c8711f35c0c5daa
stanleylew5 <stanley43595@gmail.com>
2025-05-14 14:36:20 -0700

    add a space

--- src/components/home/projectcard.tsx
+++ src/components/home/projectcard.tsx
@@ -11,7 +11,7 @@ const ProjectCard = ({ project, image, link }: ProjectCardProps) => {
   return (
     <Link href={link} className="relative flex justify-center">
       <Image src={image} alt={`${project} Image`} />
-      <div className="md:bottom-[2vh]lg:bottom-[1.7vh] absolute bottom-[2.5vh] w-11/12 rounded-3xl bg-white p-3 pl-5 text-xl font-semibold md:text-2xl">
+      <div className="absolute bottom-[1.3vh] w-11/12 rounded-3xl bg-white p-3 pl-5 text-xl font-semibold md:bottom-[2vh] md:text-2xl lg:bottom-[1.7vh]">
         {project}
       </div>
     </Link>
