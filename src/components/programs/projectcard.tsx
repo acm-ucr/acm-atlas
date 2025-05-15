edadb18d8806565fb840ef06bf4fcd4669b07b22
arkbot51 <rhahee528@gmail.com>
2025-05-15 00:38:16 -0700

    changed grid from 3 to 1 in small screens

--- src/components/programs/projectcard.tsx
+++ src/components/programs/projectcard.tsx
@@ -12,7 +12,7 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ photo, name, github, website }: ProjectCardProps) => {
   return (
-    <div className="flex w-[29vw] flex-col justify-center rounded-[2vw] border-4 border-white px-[1.6vw] py-[2vw] shadow-2xl">
+    <div className="mx-auto flex w-[70vw] flex-col justify-center rounded-[2vw] border-4 border-white px-[3vw] py-[4vw] shadow-2xl md:w-[29vw] md:px-[1.6vw] md:py-[2vw]">
       <div className="relative aspect-[16/9] w-full">
         <Image
           src={photo}
@@ -23,14 +23,16 @@ const ProjectCard = ({ photo, name, github, website }: ProjectCardProps) => {
       </div>
 
       <div className="mt-4 flex items-center justify-between text-left">
-        <div className="w-[19vw] truncate text-[2.3vw] font-light">{name}</div>
+        <div className="w-[50vw] truncate text-[4vw] font-light md:w-[19vw] md:text-[2.3vw]">
+          {name}
+        </div>
         <div className="flex items-center gap-2 text-[1.6vw]">
           {github && (
             <Link
               href={github}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[2.2vw]"
+              className="text-[4vw] md:text-[2.2vw]"
             >
               <FaGithub />
             </Link>
@@ -40,7 +42,7 @@ const ProjectCard = ({ photo, name, github, website }: ProjectCardProps) => {
               href={website}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[2.2vw]"
+              className="text-[4vw] md:text-[2.2vw]"
             >
               <FaExternalLinkAlt />
             </Link>
