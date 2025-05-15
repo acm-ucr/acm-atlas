42001b697438989ea1f634114a09f6ec511b5713
arkbot51 <rhahee528@gmail.com>
2025-05-15 02:13:46 -0700

    animated what we offer component

--- src/components/hacks/hacks.tsx
+++ src/components/hacks/hacks.tsx
@@ -1,8 +1,10 @@
+"use client";
 import hackathons from "@/data/hackathons";
 import WhatWeOffer from "./whatweoffer";
 import Aurora from "@/public/logos/aurora.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "motion/react";
 const Hacks = () => {
   return (
     <div className="flex flex-col gap-4">
@@ -20,27 +22,54 @@ const Hacks = () => {
         </p>
         <p className="my-[4vh] text-center text-5xl font-bold">WHAT WE OFFER</p>
       </div>
+
       <div className="mx-[15%] grid grid-cols-3 pb-8">
         <div className="col-span-2">
-          <p className="pb-4 text-4xl font-bold text-acm-turquoise-300">
-            AURORA
-          </p>
-          <p className="w-5/6 py-2 pb-6 text-2xl font-semibold text-acm-gray-500">
-            Aurora is an all in one dashboard used to manage and run Hackathon
-            competitions.
-          </p>
-
-          <Link
-            href="/hacks/aurora"
-            className="flex w-1/4 items-center justify-center rounded-lg bg-acm-turquoise-200 py-2 text-xl font-semibold text-white"
+          <motion.div
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 30 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            className={`pb-4 text-4xl font-bold text-acm-turquoise-300`}
           >
-            Learn More →
-          </Link>
+            AURORA
+          </motion.div>
+          <motion.div
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 30 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            <p className="w-5/6 py-2 pb-6 text-2xl font-semibold text-acm-gray-500">
+              Aurora is an all in one dashboard used to manage and run Hackathon
+              competitions.
+            </p>
+
+            <Link
+              href="/hacks/aurora"
+              className="flex w-1/4 items-center justify-center rounded-lg bg-acm-turquoise-200 py-2 text-xl font-semibold text-white"
+            >
+              Learn More →
+            </Link>
+          </motion.div>
         </div>
-        <div className="flex items-center justify-center">
+
+        <motion.div
+          viewport={{ once: true }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0, scale: 0.8 }}
+          transition={{
+            type: "spring",
+            damping: 40,
+            stiffness: 300,
+            delay: 0.5,
+          }}
+          className="flex items-center justify-center"
+        >
           <Image src={Aurora} alt="Aurora" />
-        </div>
+        </motion.div>
       </div>
+
       {hackathons.map(
         ({ description, bgColor, textColor, logo, id, name }, index) => (
           <WhatWeOffer
