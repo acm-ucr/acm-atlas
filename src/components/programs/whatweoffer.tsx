09c1cc15b72d6c04d61d21498b696fb88fa03cad
shuuvi <janeleebmes@gmail.com>
2025-05-17 19:48:58 -0700

    added animations

--- src/components/programs/whatweoffer.tsx
+++ src/components/programs/whatweoffer.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
 import Link from "next/link";
@@ -13,6 +15,21 @@ interface ProgramProps {
   link: string;
 }
 
+const textAnimation = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 30,
+  },
+};
+const imageAnimation = {
+  hidden: { opacity: 0, x: 50 },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
 const WhatWeOffer = ({
   header,
   program,
@@ -25,27 +42,52 @@ const WhatWeOffer = ({
 }: ProgramProps) => {
   return (
     <div className="grid grid-cols-1 items-center px-4 py-12 md:grid-cols-3 md:px-[15%]">
-      <div className="mb-5 mt-10 flex items-center justify-center md:order-2 md:mt-0">
+      <motion.div
+        variants={imageAnimation}
+        transition={{ duration: 0.2, delay: 0.6 }}
+        initial="hidden"
+        whileInView="show"
+        className="mb-5 mt-10 flex items-center justify-center md:order-2 md:mt-0"
+      >
         <Image src={image} alt="program" />
-      </div>
+      </motion.div>
       <div className="col-span-2">
-        <div className="flex flex-wrap justify-center text-center text-2xl font-bold text-acm-gray-500 md:justify-start md:text-left md:text-4xl">
+        <motion.div
+          className="flex flex-wrap justify-center text-center text-2xl font-bold text-acm-gray-500 md:justify-start md:text-left md:text-4xl"
+          variants={textAnimation}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          initial="hidden"
+          whileInView="show"
+        >
           <p>{header}</p>
           <p className={`${textColor} font-bold`}>&nbsp;{program}&nbsp;</p>
           <p>?</p>
-        </div>
-
-        <div className="w-full py-8 text-center text-xl font-semibold text-acm-gray-500 md:w-4/5 md:text-left md:text-2xl">
+        </motion.div>
+        <motion.div
+          variants={textAnimation}
+          transition={{ duration: 0.2, delay: 0.5 }}
+          initial="hidden"
+          whileInView="show"
+          className="w-full text-pretty py-8 text-center text-xl font-semibold text-acm-gray-500 md:w-4/5 md:text-left md:text-2xl"
+        >
           <p className="inline">Join</p>
           <p className="inline font-bold">&nbsp;{acm}&nbsp;</p>
           <p className="inline">{pText}</p>
-        </div>
-        <Link
-          href={link}
-          className={`${linkColor} mx-auto flex w-[75%] items-center justify-center rounded-lg py-2 text-xl font-semibold text-acm-gray-500 md:mx-0 md:w-1/2 2xl:w-1/4`}
+        </motion.div>
+        <motion.div
+          variants={textAnimation}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          initial="hidden"
+          whileInView="show"
+          className="mx-auto flex w-[75%] items-center justify-center rounded-lg py-2 text-xl font-semibold text-acm-gray-500 md:mx-0 md:w-1/2 2xl:w-full 2xl:items-start 2xl:justify-start"
         >
-          Learn More →
-        </Link>
+          <Link
+            href={link}
+            className={`${linkColor} mx-auto flex w-[75%] items-center justify-center rounded-lg py-2 text-xl font-semibold text-acm-gray-500 md:mx-0 md:w-1/2 2xl:w-1/4`}
+          >
+            Learn More →
+          </Link>
+        </motion.div>
       </div>
     </div>
   );
