04043b56ff53ece830a36e476722bed030669a42
Sarbesh1234 <sankarsarbesh@gmail.com>
2025-05-19 15:40:54 -0700

    fixed color issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="relative mb-[4%] flex min-h-screen flex-col items-center justify-center overflow-hidden px-4">
-      <div className="z-4 absolute right-[65%] top-[75%] hidden h-[500px] w-[500px] rounded-full bg-acm-yellow-300 opacity-70 blur-3xl sm:block" />
-      <div className="z-2 absolute right-[62%] top-[35%] hidden h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-50 blur-3xl sm:block" />
-      <div className="z-1 absolute right-[28%] top-[50%] hidden h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:block" />
-      <div className="z-5 absolute right-[-2%] top-[50%] hidden h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:block" />
-      <div className="z-3 absolute right-[8%] top-[27%] hidden h-[500px] w-[500px] rounded-full bg-acm-green-300 opacity-30 blur-3xl sm:block" />
+      <div className="z-4 absolute right-[65%] top-[75%] h-[500px] w-[500px] rounded-full bg-acm-yellow-300 opacity-70 blur-3xl sm:block" />
+      <div className="z-2 absolute right-[62%] top-[35%] h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-50 blur-3xl sm:block" />
+      <div className="z-1 absolute right-[28%] top-[50%] h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:block" />
+      <div className="z-5 absolute right-[-2%] top-[50%] h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:block" />
+      <div className="z-3 absolute right-[8%] top-[27%] h-[500px] w-[500px] rounded-full bg-acm-green-300 opacity-30 blur-3xl sm:block" />
       <Image
         src={NotFoundKeys}
         alt="404 Keys"
