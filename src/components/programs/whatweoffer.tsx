d2cec694a7bb3d4997ada0a79faee82d2ac3e0ce
shuuvi <janeleebmes@gmail.com>
2025-05-19 17:36:24 -0700

    fixed requested changes for defining animation

--- src/components/programs/whatweoffer.tsx
+++ src/components/programs/whatweoffer.tsx
@@ -16,19 +16,28 @@ interface ProgramProps {
 }
 
 const dropInAnimation = {
-  hidden: { opacity: 0, y: 20 },
-  show: {
-    opacity: 1,
-    y: 30,
+  variants: {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 30, transition: { duration: 0.5, delay: 0.2 } },
   },
+  initial: "hidden",
+  whileInView: "show",
+};
+const dropInAnimationFast = {
+  variants: {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 30, transition: { duration: 0.2, delay: 0.2 } },
+  },
+  initial: "hidden",
+  whileInView: "show",
 };
 const slideInAnimation = {
-  hidden: { opacity: 0, x: 50 },
-  show: {
-    opacity: 1,
-    x: 0,
+  variants: {
+    hidden: { opacity: 0, x: 30 },
+    show: { opacity: 1, x: 0, transition: { duration: 0.2, delay: 0.5 } },
   },
-  transition: {duration: 0.2, delay: 0.6},
+  initial: "hidden",
+  whileInView: "show",
 };
 
 const WhatWeOffer = ({
@@ -44,7 +53,7 @@ const WhatWeOffer = ({
   return (
     <div className="grid grid-cols-1 items-center px-4 py-12 md:grid-cols-3 md:px-[15%]">
       <motion.div
-        variants={slideInAnimation}
+        {...slideInAnimation}
         className="mb-5 mt-10 flex items-center justify-center md:order-2 md:mt-0"
       >
         <Image src={image} alt="program" />
@@ -52,20 +61,14 @@ const WhatWeOffer = ({
       <div className="col-span-2">
         <motion.div
           className="flex flex-wrap justify-center text-center text-2xl font-bold text-acm-gray-500 md:justify-start md:text-left md:text-4xl"
-          variants={dropInAnimation}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          initial="hidden"
-          whileInView="show"
+          {...dropInAnimationFast}
         >
           <p>{header}</p>
           <p className={`${textColor} font-bold`}>&nbsp;{program}&nbsp;</p>
           <p>?</p>
         </motion.div>
         <motion.div
-          variants={dropInAnimation}
-          transition={{ duration: 0.2, delay: 0.5 }}
-          initial="hidden"
-          whileInView="show"
+          {...dropInAnimation}
           className="w-full text-pretty py-8 text-center text-xl font-semibold text-acm-gray-500 md:w-4/5 md:text-left md:text-2xl"
         >
           <p className="inline">Join</p>
@@ -73,10 +76,7 @@ const WhatWeOffer = ({
           <p className="inline">{pText}</p>
         </motion.div>
         <motion.div
-          variants={dropInAnimation}
-          transition={{ duration: 0.5, delay: 0.5 }}
-          initial="hidden"
-          whileInView="show"
+          {...dropInAnimation}
           className="mx-auto flex w-[75%] items-center justify-center rounded-lg py-2 text-xl font-semibold text-acm-gray-500 md:mx-0 md:w-1/2 2xl:w-full 2xl:items-start 2xl:justify-start"
         >
           <Link
