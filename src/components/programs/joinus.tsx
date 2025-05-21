14a1299fb28dbce05429ee43cefdd7806d3bb19b
Vincent Dinh <vincent.dinh401@gmail.com>
2025-05-21 01:32:56 -0700

    Changed Format

--- src/components/programs/joinus.tsx
+++ src/components/programs/joinus.tsx
@@ -11,31 +11,27 @@ interface ProgramProps {
   image: StaticImageData;
 }
 
-const textVariant = {
-  hidden: { opacity: 0, y: 20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.5, ease: "easeOut", delay: 0.5 },
-  },
+// Animation variants
+const TitleAnimation = {
+  initial: { opacity: 0, y: 20 },
+  whileInView: { opacity: 1, y: 0 },
+  viewport: { once: true },
+  transition: { duration: 0.5, ease: "easeOut", delay: 0.5 },
 };
 
-const buttonVariant = {
-  hidden: { opacity: 0, scale: 0.95 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 0.5, ease: "easeOut", delay: 0 },
-  },
+const ButtonAnimation = {
+  initial: { opacity: 0, scale: 0.95 },
+  whileInView: { opacity: 1, scale: 1 },
+  viewport: { once: true },
+  whileHover: { scale: 1.03, opacity: 0.8 },
+  transition: { duration: 0.5, ease: "easeOut" },
 };
 
-const imageVariant = {
-  hidden: { opacity: 0, x: 50 },
-  visible: {
-    opacity: 1,
-    x: 0,
-    transition: { duration: 0.6, delay: 0.5 },
-  },
+const ImageAnimation = {
+  initial: { opacity: 0, x: 50 },
+  whileInView: { opacity: 1, x: 0 },
+  viewport: { once: true },
+  transition: { duration: 0.6, delay: 0.5 },
 };
 
 const Joinus = ({ textColor, backgroundColor, image }: ProgramProps) => {
@@ -43,22 +39,14 @@ const Joinus = ({ textColor, backgroundColor, image }: ProgramProps) => {
     <div className="mx-auto grid w-10/12 items-center md:grid-cols-2">
       <div className="flex flex-col">
         <motion.p
-          variants={textVariant}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
+          {...TitleAnimation}
           className="mb-4 mt-16 flex justify-center text-3xl font-semibold text-acm-gray-500 md:mt-0 md:pb-6 md:text-5xl"
         >
           JOIN NOW
         </motion.p>
+
         <div className="flex justify-center py-4 md:py-0">
-          <motion.div
-            variants={buttonVariant}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            whileHover={{ scale: 1.03, opacity: 0.8 }}
-          >
+          <motion.div {...ButtonAnimation}>
             <Link
               href="/"
               className={`rounded-lg p-2 text-2xl md:mt-0 md:px-10 md:py-4 md:text-4xl ${textColor} ${backgroundColor}`}
@@ -69,12 +57,7 @@ const Joinus = ({ textColor, backgroundColor, image }: ProgramProps) => {
         </div>
       </div>
 
-      <motion.div
-        variants={imageVariant}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-      >
+      <motion.div {...ImageAnimation}>
         <Image src={image} alt="Join Now" className="rounded-2xl md:w-10/12" />
       </motion.div>
     </div>
