8e49dd2f234dcaeb1f0d52f7a9437675b64be1d7
Sweden <sweden.agunenye1@gmail.com>
2025-05-20 23:16:59 -0700

    removed unused variables in eventcard

--- src/components/events/eventcard.tsx
+++ src/components/events/eventcard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 import { useState } from "react";
 import { Plus, Minus } from "lucide-react";
 import { motion } from "framer-motion";
@@ -30,8 +30,6 @@ const eventcard = ({
   day,
   date,
   title,
-  location,
-  time,
   description,
   isInitiallyExpanded,
 }: EventCardProps) => {
