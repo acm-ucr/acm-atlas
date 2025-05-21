59fd096ef94b936dda104938434f9d2a9913f0d3
Sweden <sweden.agunenye1@gmail.com>
2025-05-20 23:20:00 -0700

    Revert "calendar code, needs bug fixing and styling isn't correctly applied"
    This reverts commit 78959b3f5077fbe1647130b4bc297a52671da32d.
    
    revert

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,10 +1,8 @@
 import Header from "@/components/events/header";
-import Calendar from "@/components/events/calendarcall";
 const Page = () => {
   return (
     <>
       <Header />
-      <Calendar />
     </>
   );
 };
