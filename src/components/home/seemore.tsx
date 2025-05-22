98b2a474769ddd09ca15493059c1fc9ff396763f
stanleylew5 <stanley43595@gmail.com>
2025-05-22 03:24:25 -0700

    fixes to opps

--- src/components/home/seemore.tsx
+++ src/components/home/seemore.tsx
@@ -5,7 +5,7 @@ const SeeMore = () => {
     <div className="my-8 flex justify-center">
       <Link
         href="/events"
-        className="rounded-2xl bg-acm-blue-700 px-12 py-3 text-3xl font-medium text-white"
+        className="rounded-2xl bg-acm-blue-700 px-12 py-3 text-3xl font-medium text-white hover:scale-105 hover:opacity-80"
       >
         See More
       </Link>
