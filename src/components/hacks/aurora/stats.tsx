26832b8bf4b6914fd9a06540bfc787d55def0380
j1munoz <jimmymunoz397@gmail.com>
2025-05-26 15:44:00 -0700

    Merge conflicts

--- src/components/hacks/aurora/stats.tsx
+++ src/components/hacks/aurora/stats.tsx
@@ -1,38 +1,65 @@
 "use client";
+
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import auroraStar from "@/public/logos/aurorastar.webp";
-import { useQuery } from "@tanstack/react-query";
+
+const fetchRepoStats = async () => {
+  const res = await fetch("https://api.github.com/repos/acm-ucr/aurora");
+  if (!res.ok) throw new Error("Failed to fetch repo data");
+  const data = await res.json();
+
+  const closedPRs = await fetchClosedPRs();
+  if (!closedPRs) throw new Error("Failed to fetch closed PRs");
+
+  const createdAt = new Date(data.created_at);
+  const now = new Date();
+  const timeDiff = now.getTime() - createdAt.getTime();
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  const commitsRes = await fetch(
+    "https://api.github.com/repos/acm-ucr/aurora/commits?per_page=1",
+  );
+  const linkHeader = commitsRes.headers.get("Link");
+  const extractHeader = linkHeader?.match(/&page=(\d+)>; rel="last"/);
+  const extractCommitCount = extractHeader?.at(1);
+
+  return {
+    commits: extractCommitCount,
+    days,
+    closedPRs,
+  };
+};
+
+const fetchClosedPRs = async () => {
+  const res = await fetch(
+    "https://api.github.com/search/issues?q=repo:acm-ucr/aurora+type:pr+state:closed",
+  );
+
+  const data = await res.json();
+  return data.total_count;
+};
 
 const Stats = () => {
-  const fetchClosedPRs = async () => {
-    const res = await fetch(
-      "https://api.github.com/search/issues?q=repo:acm-ucr/aurora+type:pr+state:closed",
-    );
-
-    const data = await res.json();
-    return data.total_count;
-  };
-
-  const {
-    data: closedPRs,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["closedPRs"],
-    queryFn: fetchClosedPRs,
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["repoStats"],
+    queryFn: fetchRepoStats,
     staleTime: 1000 * 60 * 5,
   });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError || !data) return <p>Error loading repo stats.</p>;
 
   return (
     <div className="py-30 flex flex-col justify-center">
       <div className="ml-[15%] grid gap-10 pb-[5vh] text-acm-gray-500 [grid-template-columns:23%_16%_23%]">
         <div className="flex flex-col justify-center border-r-2 border-acm-gray-100">
-          <p className="pb-4 text-6xl font-bold">4000+</p>
+          <p className="pb-4 text-6xl font-bold">{data.commits}</p>
           <p className="text-2xl font-medium">commits</p>
         </div>
         <div className="flex flex-col justify-center border-r-2 border-acm-gray-100">
           <p className="pb-4 text-6xl font-bold">
-            {isLoading ? "0" : isError ? "Error" : closedPRs}
+            {isLoading ? "0" : isError ? "Error" : data.closedPRs}
           </p>
           <p className="text-2xl font-medium">PRs</p>
         </div>
@@ -54,7 +81,7 @@ const Stats = () => {
           </div>
         </div>
         <div className="flex flex-col justify-center">
-          <p className="pb-4 text-6xl font-bold">623</p>
+          <p className="pb-4 text-6xl font-bold">{data.days}</p>
           <p className="text-2xl font-medium">days since we began</p>
         </div>
       </div>
