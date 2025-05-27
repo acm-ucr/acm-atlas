295c961d0b615ca18b75214ed27840c3b3a8bd76
Andres Lopez <alope759@ucr.edu>
2025-05-27 12:09:38 -0700

    Andres Lopez / Animated Project Cards

--- src/components/home/featuredprojects.tsx
+++ src/components/home/featuredprojects.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Link from "next/link";
 import ProjectCard from "./projectcard";
 import Spark1 from "@/public/home/featured/pad.webp";
@@ -9,6 +11,11 @@ import Create3 from "@/public/home/featured/nsu.webp";
 import Forge1 from "@/public/home/featured/wzchess.webp";
 import Forge2 from "@/public/home/featured/drone.webp";
 import Forge3 from "@/public/home/featured/blank.webp";
+
+const hoverOver = () => ({
+  whileHover: { scale: 1.05 },
+  whileTap: { scale: 0.95 },
+});
 
 const FeaturedProjects = () => {
   const index1 = 1;
@@ -25,19 +32,25 @@ const FeaturedProjects = () => {
       <div className="mx-10 mt-10 font-bold text-acm-gray-500">
         <p className="pb-3 text-center text-4xl md:text-left">ACM SPARK</p>
         <div className="grid grid-cols-1 gap-8 text-lg md:grid-cols-3">
-          <ProjectCard
-            project="PHI ALPHA DELTA"
-            image={Spark1}
-            link="/"
-            index={index1}
-          />
-          <ProjectCard
-            project="AVIAT’R"
-            image={Spark2}
-            link="/"
-            index={index2}
-          />
-          <ProjectCard project="SAF" image={Spark3} link="/" index={index3} />
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="PHI ALPHA DELTA"
+              image={Spark1}
+              link="/"
+              index={index1}
+            />
+          </motion.div>
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="AVIAT’R"
+              image={Spark2}
+              link="/"
+              index={index2}
+            />
+          </motion.div>
+          <motion.div {...hoverOver()}>
+            <ProjectCard project="SAF" image={Spark3} link="/" index={index3} />
+          </motion.div>
         </div>
         <Link
           href="/"
@@ -47,9 +60,30 @@ const FeaturedProjects = () => {
         </Link>
         <p className="pb-3 text-center text-4xl md:text-left">ACM CREATE</p>
         <div className="grid grid-cols-1 gap-8 text-lg md:grid-cols-3">
-          <ProjectCard project="ACM" image={Create1} link="/" index={index1} />
-          <ProjectCard project="WITL" image={Create2} link="/" index={index2} />
-          <ProjectCard project="NSU" image={Create3} link="/" index={index3} />
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="ACM"
+              image={Create1}
+              link="/"
+              index={index1}
+            />
+          </motion.div>
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="WITL"
+              image={Create2}
+              link="/"
+              index={index2}
+            />
+          </motion.div>
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="NSU"
+              image={Create3}
+              link="/"
+              index={index3}
+            />
+          </motion.div>
         </div>
         <Link
           href="/"
@@ -59,19 +93,30 @@ const FeaturedProjects = () => {
         </Link>
         <p className="pb-3 text-center text-4xl md:text-left">ACM FORGE</p>
         <div className="grid grid-cols-1 gap-8 text-lg md:grid-cols-3">
-          <ProjectCard
-            project="WIZARD CHESS"
-            image={Forge1}
-            link="/"
-            index={index1}
-          />
-          <ProjectCard
-            project="DRONES"
-            image={Forge2}
-            link="/"
-            index={index2}
-          />
-          <ProjectCard project="ARMS" image={Forge3} link="/" index={index3} />
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="WIZARD CHESS"
+              image={Forge1}
+              link="/"
+              index={index1}
+            />
+          </motion.div>
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="DRONES"
+              image={Forge2}
+              link="/"
+              index={index2}
+            />
+          </motion.div>
+          <motion.div {...hoverOver()}>
+            <ProjectCard
+              project="ARMS"
+              image={Forge3}
+              link="/"
+              index={index3}
+            />
+          </motion.div>
         </div>
         <Link
           href="/"
