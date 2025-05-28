1605242499539797eebdd24fb8d18e5c55101725
stanleylew5 <stanley43595@gmail.com>
2025-05-28 15:10:10 -0700

    fix imagecard

--- src/components/join/imagecard.tsx
+++ src/components/join/imagecard.tsx
@@ -8,14 +8,16 @@ interface ImageCard {
 
 const ImageCard = ({ image, alt, description }: ImageCard) => {
   return (
-    <div className="group relative h-full p-2 text-acm-blue-500 sm:p-0">
+    <div className="group relative h-full text-acm-blue-500">
       <Image
         src={image}
         alt={alt}
         className="h-full w-full rounded-3xl object-cover"
       />
       <div className="absolute bottom-0 z-10 flex h-1/3 w-full items-end rounded-3xl bg-blue-fade text-right font-bold opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-        <div className="w-full p-4 text-white md:text-3xl">{description}</div>
+        <div className="w-full p-4 text-xl text-white md:text-3xl">
+          {description}
+        </div>
       </div>
     </div>
   );
