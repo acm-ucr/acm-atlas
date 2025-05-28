692f8da4d237cd357a500e76bac69b3d8e3df08f
Shaodw709 <amirvs9675@gmail.com>
2025-05-27 23:51:54 -0700

    add forge

--- src/components/programs/forge/stats.tsx
+++ src/components/programs/forge/stats.tsx
@@ -6,27 +6,27 @@ const StatsGrid: React.FC = () => {
     <div>
       <div className="mx-auto hidden w-3/4 justify-center py-12 text-left md:flex">
         <div className="pr-12">
-          <StatItem end={93} label="quarterly applications" duration={0.7} />
+          <StatItem end={62} label="quarterly applications" duration={0.7} />
         </div>
         <div className="border-l-2 pl-4 pr-8">
-          <StatItem end={29} label="total acceptances" duration={0.7} />
+          <StatItem end={32} label="total acceptances" duration={0.7} />
         </div>
         <div className="border-l-2 pl-4 pr-12">
-          <StatItem end={4} label="projects" duration={0.7} />
+          <StatItem end={3} label="projects" duration={0.7} />
         </div>
       </div>
       <div className="flex flex-col md:hidden">
         <div className="flex justify-center py-12 text-left">
           <div className="pr-12">
-            <StatItem end={93} label="quarterly applications" duration={0.7} />
+            <StatItem end={62} label="quarterly applications" duration={0.7} />
           </div>
           <div className="border-l-2 pl-4 pr-8">
-            <StatItem end={29} label="total acceptances" duration={0.7} />
+            <StatItem end={32} label="total acceptances" duration={0.7} />
           </div>
         </div>
         <div className="flex justify-center">
           <div className="pl-4 pr-12">
-            <StatItem end={4} label="projects" duration={0.7} />
+            <StatItem end={3} label="projects" duration={0.7} />
           </div>
         </div>
       </div>
