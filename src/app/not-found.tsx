b1c0a9d85cea7492b9feaa7b7f854a0f590c71ff
Sarbesh1234 <sankarsarbesh@gmail.com>
2025-05-29 14:23:05 -0700

    fixed mobile responsive changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="relative mb-[4%] flex min-h-screen flex-col items-center justify-center overflow-hidden px-4">
-      <div className="z-4 absolute right-[65%] top-[75%] h-[500px] w-[500px] rounded-full bg-acm-yellow-300 opacity-70 blur-3xl sm:block" />
-      <div className="z-2 absolute right-[62%] top-[35%] h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-50 blur-3xl sm:block" />
-      <div className="z-1 absolute right-[28%] top-[50%] h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:block" />
-      <div className="z-5 absolute right-[-2%] top-[50%] h-[500px] w-[500px] rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:block" />
-      <div className="z-3 absolute right-[8%] top-[27%] h-[500px] w-[500px] rounded-full bg-acm-green-300 opacity-30 blur-3xl sm:block" />
+      <div className="absolute right-4 top-2/3 block h-32 w-32 rounded-full bg-acm-yellow-300 opacity-70 blur-3xl sm:right-[65%] sm:top-[75%] sm:h-[500px] sm:w-[500px]" />
+      <div className="absolute right-8 top-1/3 block h-32 w-32 rounded-full bg-acm-blue-700 opacity-50 blur-3xl sm:right-[62%] sm:top-[35%] sm:h-[500px] sm:w-[500px]" />
+      <div className="absolute right-1/4 top-1/2 block h-24 w-24 rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:right-[28%] sm:top-[50%] sm:h-[500px] sm:w-[500px]" />
+      <div className="absolute right-0 top-1/2 block h-24 w-24 rounded-full bg-acm-blue-700 opacity-40 blur-3xl sm:right-[-2%] sm:top-[50%] sm:h-[500px] sm:w-[500px]" />
+      <div className="absolute right-8 top-1/4 block h-20 w-20 rounded-full bg-acm-green-300 opacity-30 blur-3xl sm:right-[8%] sm:top-[27%] sm:h-[500px] sm:w-[500px]" />
       <Image
         src={NotFoundKeys}
         alt="404 Keys"
