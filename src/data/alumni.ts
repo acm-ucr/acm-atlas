6d03c5cf2a64de3fe212597edf45c262d7347192
stanleylew5 <stanley43595@gmail.com>
2025-06-02 12:18:06 -0700

    add more people

--- src/data/alumni.ts
+++ src/data/alumni.ts
@@ -396,6 +396,13 @@ const alumni = [
         color: "text-acm-yellow-500",
       },
       {
+        name: "Mailyn Nguyen",
+        position: "Spark Lead",
+        linkedin: "https://www.linkedin.com/in/mailyn-nguyen",
+        github: "https://github.com/mailynnguyen",
+        color: "text-acm-yellow-500",
+      },
+      {
         name: "Felix Diaz",
         position: "Spark Lead",
         linkedin: "https://www.linkedin.com/in/felix-m-diaz",
@@ -512,6 +519,13 @@ const alumni = [
         position: "Spark Lead",
         linkedin: "https://www.linkedin.com/in/minh-tran-677564332/",
         github: "https://github.com/minhhdtran",
+        color: "text-acm-yellow-500",
+      },
+      {
+        name: "Brandon Sun",
+        position: "Spark Lead",
+        linkedin: "https://www.linkedin.com/in/sunbrandon",
+        github: "https://github.com/sunbrandon",
         color: "text-acm-yellow-500",
       },
       {
