4c12c4133cd6c63791d0ca12893a1f66f60bd459
j1munoz <jimmymunoz397@gmail.com>
2025-06-03 22:53:29 -0700

    Hover animations

--- src/components/home/opportunitiescard.tsx
+++ src/components/home/opportunitiescard.tsx
@@ -19,6 +19,14 @@ const dropInAnim = (index: number) => ({
   viewport: { once: true },
 });
 
+const ButtonAnimation = {
+  initial: { opacity: 0, scale: 0.95 },
+  whileInView: { opacity: 1, scale: 1 },
+  viewport: { once: true },
+  whileHover: { scale: 1.03, opacity: 0.8 },
+  transition: { duration: 0.5, ease: "easeOut" },
+};
+
 const OpportunitiesCard = ({
   name,
   cardBackground,
@@ -55,20 +63,22 @@ const OpportunitiesCard = ({
           </Link>
         </div>
         <div className="absolute bottom-5 left-0 flex w-full justify-center">
-          {applicationLink !== "/" ? (
-            <Link
-              href={applicationLink}
-              className="rounded-full bg-white px-9 py-2 text-lg font-bold text-acm-gray-500"
-            >
-              {appYearAndQuarter}
-              {isOpen()}
-            </Link>
-          ) : (
-            <p className="cursor-not-allowed rounded-full bg-white px-9 py-2 text-lg font-bold text-acm-gray-500">
-              {appYearAndQuarter}
-              {isOpen()}
-            </p>
-          )}
+          <motion.div {...ButtonAnimation}>
+            {applicationLink !== "/" ? (
+              <Link
+                href={applicationLink}
+                className="rounded-full bg-white px-9 py-2 text-lg font-bold text-acm-gray-500"
+              >
+                {appYearAndQuarter}
+                {isOpen()}
+              </Link>
+            ) : (
+              <p className="cursor-not-allowed rounded-full bg-white px-9 py-2 text-lg font-bold text-acm-gray-500">
+                {appYearAndQuarter}
+                {isOpen()}
+              </p>
+            )}
+          </motion.div>
         </div>
       </div>
     </motion.div>
