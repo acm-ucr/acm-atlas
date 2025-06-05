41b3e57d130b9fe28c86d5b0972fe1f081a2ed44
stanleylew5 <stanley43595@gmail.com>
2025-06-05 13:22:38 -0700

    make text slightly larger

--- src/components/home/statsgrid.tsx
+++ src/components/home/statsgrid.tsx
@@ -4,15 +4,15 @@ import StatItem from "@/components/statitem";
 const StatsGrid: React.FC = () => {
   return (
     <div>
-      <div className="mx-auto flex flex-col items-center py-12 text-center md:flex-row md:justify-center md:text-left">
+      <div className="mx-auto flex flex-col items-center gap-y-4 py-12 text-center md:flex-row md:justify-center md:text-left">
         <div className="pr-0 text-center md:pr-12 md:text-base">
           <StatItem
             end={500}
             label="members"
             duration={0.7}
             color="text-acm-blue-600"
-            numberSize="text-4xl md:text-7xl"
-            labelSize="text-xl md:text-3xl"
+            numberSize="text-5xl md:text-7xl"
+            labelSize="text-2xl md:text-3xl"
           />
         </div>
         <div className="border-0 pl-0 pr-0 pt-4 text-center md:border-l-2 md:pl-4 md:pr-8 md:pt-0 md:text-base">
@@ -21,8 +21,8 @@ const StatsGrid: React.FC = () => {
             label="alumni"
             duration={0.7}
             color="text-acm-green-500"
-            numberSize="text-4xl md:text-7xl"
-            labelSize="text-xl md:text-3xl"
+            numberSize="text-5xl md:text-7xl"
+            labelSize="text-2xl md:text-3xl"
           />
         </div>
         <div className="border-0 pl-0 pr-0 pt-4 text-center md:border-l-2 md:pl-4 md:pr-12 md:pt-0 md:text-base">
@@ -31,8 +31,8 @@ const StatsGrid: React.FC = () => {
             label="socials"
             duration={0.7}
             color="text-acm-yellow-500"
-            numberSize="text-4xl md:text-7xl"
-            labelSize="text-xl md:text-3xl"
+            numberSize="text-5xl md:text-7xl"
+            labelSize="text-2xl md:text-3xl"
           />
         </div>
         <div className="border-0 pl-0 pt-4 text-center md:border-l-2 md:pl-4 md:pt-0 md:text-base">
@@ -41,8 +41,8 @@ const StatsGrid: React.FC = () => {
             label="workshops"
             duration={0.7}
             color="text-acm-purple-200"
-            numberSize="text-4xl md:text-7xl"
-            labelSize="text-xl md:text-3xl"
+            numberSize="text-5xl md:text-7xl"
+            labelSize="text-2xl md:text-3xl"
           />
         </div>
       </div>
