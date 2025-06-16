302fd157f4312cfa7ad6ce60463575b5b8476988
stanleylew5 <stanley43595@gmail.com>
2025-06-16 02:35:14 -0700

    fix carousel items

--- src/components/programs/carouselitem.tsx
+++ src/components/programs/carouselitem.tsx
@@ -11,7 +11,7 @@ const CarouselItem = ({ name, icon, borderColor }: CarouselItemProps) => {
     <div
       className={`${borderColor} absolute left-[100%] flex w-64 items-center justify-center rounded-lg border-2 px-5 py-3 pr-14 md:w-80`}
     >
-      <Image src={icon} alt="icon" className="w-12 pr-4 md:w-16" />
+      <Image src={icon} alt="icon" className="h-12 pr-4 md:w-16" />
       <p className="text-xl font-light md:text-3xl">{name}</p>
     </div>
   );
